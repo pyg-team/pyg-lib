9668811a2789d8be29c4942b71040c533732ccc5
rusty1s <matthias.fey@tu-dortmund.de>
2022-03-27 09:03:42 +0000

    cuda version impl

---
+++ pyg_lib/csrc/pyg.h
@@ -0,0 +1,17 @@
+#pragma once
+
+#include "macros.h"
+
+namespace pyg {
+
+PYG_API int64_t cuda_version();
+
+namespace detail {
+
+extern "C" PYG_INLINE_VARIABLE auto _register_ops = &cuda_version;
+#ifdef HINT_MSVC_LINKER_INCLUDE_SYMBOL
+#pragma comment(linker, "/include:_register_ops")
+#endif
+
+} // namespace detail
+} // namespace pyg
