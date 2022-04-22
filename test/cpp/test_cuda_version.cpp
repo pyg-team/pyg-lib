d20c0e88a7ee2473f1bfe07ff53f5113d371316f
Zeyuan Tan <41138939+ZenoTan@users.noreply.github.com>
2022-04-22 18:20:07 +0100

    Initialize cpp test with `gtest` and github action (#13)
    * update
    
    * torch style
    
    * fix
    
    * ci
    
    * Update CMakeLists.txt
    
    Co-authored-by: Matthias Fey <matthias.fey@tu-dortmund.de>
    
    * Update CMakeLists.txt
    
    Co-authored-by: Matthias Fey <matthias.fey@tu-dortmund.de>
    
    * Update test/cpp/test_cuda_version.cpp
    
    Co-authored-by: Matthias Fey <matthias.fey@tu-dortmund.de>
    
    * Update cmake/test.cmake
    
    Co-authored-by: Matthias Fey <matthias.fey@tu-dortmund.de>
    
    * update
    
    * doc
    
    Co-authored-by: Matthias Fey <matthias.fey@tu-dortmund.de>

---
+++ test/cpp/test_cuda_version.cpp
@@ -0,0 +1,11 @@
+#include <gtest/gtest.h>
+
+#include "../../pyg_lib/csrc/library.h"
+
+TEST(CudaVersionTest, BasicAssertions) {
+#ifdef WITH_CUDA
+  EXPECT_NE(pyg::cuda_version(), -1);
+#else
+  EXPECT_EQ(pyg::cuda_version(), -1);
+#endif
+}
