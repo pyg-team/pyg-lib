f9b9929bfb815cc504e4d3630db861947500f1de
rusty1s <matthias.fey@tu-dortmund.de>
2022-04-25 09:03:34 +0200

    update

--- pyg_lib/csrc/sampler/random_walk.cpp
+++ pyg_lib/csrc/sampler/random_walk.cpp
@@ -1,17 +1,22 @@
 #include "random_walk.h"
 
-#include <ATen/core/dispatch/Dispatcher.h>
-#include <torch/library.h>
-
 namespace pyg {
 namespace sampler {
 
-at::Tensor random_walk(const at::Tensor& rowptr,
-                       const at::Tensor& col,
-                       const at::Tensor& seed,
-                       int64_t walk_length,
-                       double p,
-                       double q) {
+torch::Tensor random_walk(const torch::Tensor& rowptr,
+                          const torch::Tensor& col,
+                          const torch::Tensor& seed,
+                          int64_t walk_length,
+                          double p,
+                          double q) {
+  at::TensorArg rowptr_t{rowptr, "rowtpr", 1};
+  at::TensorArg col_t{col, "col", 1};
+  at::TensorArg seed_t{seed, "seed", 1};
+
+  at::CheckedFrom c = "random_walk";
+  at::checkAllDefined(c, {rowptr_t, col_t, seed_t});
+  at::checkAllSameType(c, {rowptr_t, col_t, seed_t});
+
   static auto op = c10::Dispatcher::singleton()
                        .findSchemaOrThrow("pyg::random_walk", "")
                        .typed<decltype(random_walk)>();
--- test/csrc/sampler/test_random_walk.cpp
+++ test/csrc/sampler/test_random_walk.cpp
@@ -2,8 +2,20 @@
 
 #include "../../../pyg_lib/csrc/sampler/random_walk.h"
 
-#include <iostream>
-
 TEST(RandomWalkTest, BasicAssertions) {
-  std::cout << "random walk test" << std::endl;
+  const auto options = torch::TensorOptions().dtype(torch::kInt64);
+  const auto rowptr = torch::tensor({0, 2, 4, 6, 8}, options);
+  const auto col = torch::tensor({1, 3, 0, 2, 1, 3, 2, 0}, options);
+  const auto seed = torch::arange(4, options);
+
+  /* std::cout << rowptr << std::endl; */
+  /* std::cout << col << std::endl; */
+  /* std::cout << seed << std::endl; */
+
+  auto out = pyg::sampler::random_walk(rowptr, col, seed, /*walk_length=*/3);
+
+  /* row = tensor([ 0, 1, 1, 1, 2, 2, 3, 3, 4, 4 ], torch.long, device) col = */
+  /*     tensor([ 1, 0, 2, 3, 1, 4, 1, 4, 2, 3 ], torch.long, device) */
+
+  /* torch::Tensor */
 }
