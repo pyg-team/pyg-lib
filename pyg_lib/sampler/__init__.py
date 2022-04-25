e59f0703917b3584ed3537829cd8d147efddfe09
rusty1s <matthias.fey@tu-dortmund.de>
2022-04-25 09:29:05 +0200

    update

---
+++ pyg_lib/sampler/__init__.py
@@ -0,0 +1,28 @@
+import torch
+from torch import Tensor
+
+
+def random_walk(rowptr: Tensor, col: Tensor, seed: Tensor, walk_length: int,
+                p: float = 1.0, q: float = 1.0) -> Tensor:
+    r"""Samples random walks of length :obj:`walk_length` from all node
+    indices in :obj:`seed` in the graph given by :obj:`(rowptr, col)`, as
+    described in the `"node2vec: Scalable Feature Learning for Networks"
+    <https://arxiv.org/abs/1607.00653>`_ paper.
+
+    Args:
+        rowptr (torch.Tensor): Compressed source node indices.
+        col (torch.Tensor): Target node indices.
+        seed (torch.Tensor): Seed node indices from where random walks start.
+        walk_length (int): The walk length of a random walk.
+        p (float, optional): Likelihood of immediately revisiting a node in the
+            walk. (default: :obj:`1.0`)
+        q (float, optional): Control parameter to interpolate between
+            breadth-first strategy and depth-first strategy
+            (default: :obj:`1.0`)
+    """
+    return torch.ops.pyg.random_walk(rowptr, col, seed, walk_length, p, q)
+
+
+__all__ = [
+    'random_walk',
+]
