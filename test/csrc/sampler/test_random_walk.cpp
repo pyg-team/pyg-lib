2134cd38b98d3f5fe0d7d498d9b47bcd9769678e
rusty1s <matthias.fey@tu-dortmund.de>
2022-04-25 10:05:50 +0200

    update

--- test/csrc/sampler/test_random_walk.cpp
+++ test/csrc/sampler/test_random_walk.cpp
@@ -3,10 +3,10 @@
 #include "../../../pyg_lib/csrc/sampler/random_walk.h"
 
 TEST(RandomWalkTest, BasicAssertions) {
-  const auto options = torch::TensorOptions().dtype(torch::kInt64);
-  const auto rowptr = torch::tensor({0, 2, 4, 6, 8}, options);
-  const auto col = torch::tensor({1, 3, 0, 2, 1, 3, 2, 0}, options);
-  const auto seed = torch::arange(4, options);
+  /* const auto options = torch::TensorOptions().dtype(torch::kInt64); */
+  /* const auto rowptr = torch::tensor({0, 2, 4, 6, 8}, options); */
+  /* const auto col = torch::tensor({1, 3, 0, 2, 1, 3, 2, 0}, options); */
+  /* const auto seed = torch::arange(4, options); */
 
   // auto out = pyg::sampler::random_walk(rowptr, col, seed, /*walk_length=*/5);
 
