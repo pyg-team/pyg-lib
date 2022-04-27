0103eaa049a5d27a767db44b0dfb05db900914d6
rusty1s <matthias.fey@tu-dortmund.de>
2022-04-27 17:16:23 +0000

    update

--- .github/workflows/cuda/cu102-Linux-env.sh
+++ .github/workflows/cuda/cu102-Linux-env.sh
@@ -7,3 +7,6 @@ export PATH=${CUDA_HOME}/bin:${PATH}
 export USE_CUDNN=0
 export FORCE_CUDA=1
 export CUDA_ARCH_LIST="3.5;5.0+PTX;6.0;7.0;7.5"
+
+export CUDNN_LIBRARY_PATH=${CUDA_HOME}/lib64
+export CUDNN_INCLUDE_PATH=${CUDA_HOME}/include
