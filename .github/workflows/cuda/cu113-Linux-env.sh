a1844e8214739c5cb51f46a02c06c20a9ef0f9cf
rusty1s <matthias.fey@tu-dortmund.de>
2022-04-28 08:11:28 +0000

    add path

--- .github/workflows/cuda/cu113-Linux-env.sh
+++ .github/workflows/cuda/cu113-Linux-env.sh
@@ -1,7 +1,4 @@
 #!/bin/bash
 
 export CUDA_HOME=/usr/local/cuda-11.3
-export LD_LIBRARY_PATH=${CUDA_HOME}/lib64:${LD_LIBRARY_PATH}
-# export PATH=${CUDA_HOME}/bin:${PATH}
-
-export FORCE_CUDA=1
+export PATH=${CUDA_HOME}/bin:${PATH}
--- .github/workflows/cuda/cu115-Linux-env.sh
+++ .github/workflows/cuda/cu115-Linux-env.sh
@@ -2,6 +2,6 @@
 
 export CUDA_HOME=/usr/local/cuda-11.5
 # export LD_LIBRARY_PATH=${CUDA_HOME}/lib64:${LD_LIBRARY_PATH}
-# export PATH=${CUDA_HOME}/bin:${PATH}
+export PATH=${CUDA_HOME}/bin:${PATH}
 
 export FORCE_CUDA=1
