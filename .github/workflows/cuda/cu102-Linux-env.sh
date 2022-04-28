5bc35b13f2d380b2ada969605f5333e10416c42e
rusty1s <matthias.fey@tu-dortmund.de>
2022-04-28 07:55:10 +0000

    update

--- .github/workflows/cuda/cu102-Linux-env.sh
+++ .github/workflows/cuda/cu102-Linux-env.sh
@@ -1,12 +1,7 @@
 #!/bin/bash
 
 export CUDA_HOME=/usr/local/cuda-10.2
-export LD_LIBRARY_PATH=${CUDA_HOME}/lib64:${LD_LIBRARY_PATH}
+# export LD_LIBRARY_PATH=${CUDA_HOME}/lib64:${LD_LIBRARY_PATH}
 export PATH=${CUDA_HOME}/bin:${PATH}
 
-export USE_CUDNN=0
 export FORCE_CUDA=1
-export CUDA_ARCH_LIST="3.5;5.0+PTX;6.0;7.0;7.5"
-
-export CUDNN_LIBRARY_PATH=${CUDA_HOME}/lib64
-export CUDNN_INCLUDE_PATH=${CUDA_HOME}/include
--- .github/workflows/cuda/cu113-Linux-env.sh
+++ .github/workflows/cuda/cu113-Linux-env.sh
@@ -2,8 +2,6 @@
 
 export CUDA_HOME=/usr/local/cuda-11.3
 export LD_LIBRARY_PATH=${CUDA_HOME}/lib64:${LD_LIBRARY_PATH}
-export PATH=${CUDA_HOME}/bin:${PATH}
+# export PATH=${CUDA_HOME}/bin:${PATH}
 
-export USE_CUDNN=0
 export FORCE_CUDA=1
-export CUDA_ARCH_LIST="3.5;5.0+PTX;6.0;7.0;7.5;8.0;8.6"
