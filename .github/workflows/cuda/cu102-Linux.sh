a59e6651f4d688150043249825bcf1e05fb7fffc
rusty1s <matthias.fey@tu-dortmund.de>
2022-04-28 07:11:24 +0000

    update

--- .github/workflows/cuda/cu102-Linux.sh
+++ .github/workflows/cuda/cu102-Linux.sh
@@ -6,12 +6,13 @@ wget -nv https://developer.download.nvidia.com/compute/cuda/repos/${OS}/x86_64/c
 sudo mv cuda-${OS}.pin /etc/apt/preferences.d/cuda-repository-pin-600
 
 CUDA=10.2
-FILENAME=cuda-repo-${OS}-${CUDA/./-}-local-10.2.89-440.33.01_1.0-1_amd64.deb
+APT_KEY=${CUDA/./-}-local-10.2.89-440.33.01
+FILENAME=cuda-repo-${OS}-${APT_KEY}_1.0-1_amd64.deb
 URL=https://developer.download.nvidia.com/compute/cuda/10.2/Prod/local_installers
 
 wget -nv ${URL}/${FILENAME}
 sudo dpkg -i ${FILENAME}
-sudo apt-key add /var/cuda-repo-${CUDA/./-}-local/7fa2af80.pub
+sudo apt-key add /var/cuda-repo-${APT_KEY}/7fa2af80.pub
 
 sudo apt-get -qq update
 sudo apt install cuda-nvcc-${CUDA/./-}- cuda-libraries-dev-${CUDA/./-} cuda-command-line-tools-${CUDA/./-}
--- .github/workflows/cuda/cu115-Linux.sh
+++ .github/workflows/cuda/cu115-Linux.sh
@@ -6,12 +6,13 @@ wget -nv https://developer.download.nvidia.com/compute/cuda/repos/${OS}/x86_64/c
 sudo mv cuda-${OS}.pin /etc/apt/preferences.d/cuda-repository-pin-600
 
 CUDA=11.5
-FILENAME=cuda-repo-${OS}-${CUDA/./-}-local_11.5.2-495.29.05-1_amd64.deb
+APT_KEY=${OS}-${CUDA/./-}-local
+FILENAME=cuda-repo-${APT_KEY}_11.5.2-495.29.05-1_amd64.deb
 URL=https://developer.download.nvidia.com/compute/cuda/11.5.2/local_installers
 
 wget -nv ${URL}/${FILENAME}
 sudo dpkg -i ${FILENAME}
-sudo apt-key add /var/cuda-repo-${OS}-${CUDA/./-}-local/7fa2af80.pub
+sudo apt-key add /var/cuda-repo-${APT_KEY}/7fa2af80.pub
 
 sudo apt-get -qq update
 sudo apt install cuda-nvcc-${CUDA/./-}- cuda-libraries-dev-${CUDA/./-} cuda-command-line-tools-${CUDA/./-}
