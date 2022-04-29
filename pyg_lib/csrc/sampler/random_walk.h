e1267ea9754542afba6e3c2aacf26f81cf4ae664
rusty1s <matthias.fey@tu-dortmund.de>
2022-04-29 11:13:21 +0200

    typo

--- pyg_lib/csrc/sampler/random_walk.h
+++ pyg_lib/csrc/sampler/random_walk.h
@@ -7,7 +7,7 @@ namespace pyg {
 namespace sampler {
 
 // Samples random walks of length `walk_length` from all nodes indices in `seed`
-// in the graph given by `(rowptr, col`.
+// in the graph given by `(rowptr, col)`.
 // Returns: a tensor of shape `[seed.size(0), walk_length + 1]`
 PYG_API at::Tensor random_walk(const at::Tensor& rowptr,
                                const at::Tensor& col,
