0ee65f6b9bc95e70285d455217563194388b8414
rusty1s <matthias.fey@tu-dortmund.de>
2022-05-02 11:15:50 +0200

    update

--- test/csrc/sampler/test_subgraph.cpp
+++ test/csrc/sampler/test_subgraph.cpp
@@ -17,6 +17,6 @@ TEST(SubgraphTest, BasicAssertions) {
   EXPECT_TRUE(at::equal(std::get<0>(out), expected_rowptr));
   auto expected_col = at::tensor({1, 0, 2, 1, 3, 2}, options);
   EXPECT_TRUE(at::equal(std::get<1>(out), expected_col));
-  /* auto expected_edge_id = at::tensor({3, 4, 5, 6, 7, 8}, options); */
-  /* EXPECT_TRUE(at::equal(std::get<2>(out), expected_edge_id)); */
+  auto expected_edge_id = at::tensor({3, 4, 5, 6, 7, 8}, options);
+  EXPECT_TRUE(at::equal(std::get<2>(out).value(), expected_edge_id));
 }
