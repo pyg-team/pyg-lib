84874f161229850ace13357dc93c128af619bc02
Matthias Fey <matthias.fey@tu-dortmund.de>
2022-05-03 14:00:06 +0200

    Auto-compute number of threads and blocks in CUDA kernels (#41)
    * auto-compute number of threads and blocks
    
    * update

--- pyg_lib/csrc/sampler/cuda/random_walk_kernel.cu
+++ pyg_lib/csrc/sampler/cuda/random_walk_kernel.cu
@@ -7,8 +7,18 @@ namespace sampler {
 
 namespace {
 
-#define THREADS 1024
-#define BLOCKS(N) (N + THREADS - 1) / THREADS
+int threads() {
+  const auto props = at::cuda::getCurrentDeviceProperties();
+  return std::min(props->maxThreadsPerBlock, 1024);
+}
+
+int blocks(int numel) {
+  const auto props = at::cuda::getCurrentDeviceProperties();
+  const auto blocks_per_sm = props->maxThreadsPerMultiProcessor / 256;
+  const auto max_blocks = props->multiProcessorCount * blocks_per_sm;
+  const auto max_threads = threads();
+  return std::min(max_blocks, (numel + max_threads - 1) / max_threads);
+}
 
 #define CUDA_1D_KERNEL_LOOP(scalar_t, i, n)                           \
   for (scalar_t i = (blockIdx.x * blockDim.x) + threadIdx.x; i < (n); \
@@ -64,7 +74,7 @@ at::Tensor random_walk_kernel(const at::Tensor& rowptr,
     const auto rand_data = rand.data_ptr<float>();
     auto out_data = out.data_ptr<scalar_t>();
 
-    random_walk_kernel_impl<<<BLOCKS(seed.size(0)), THREADS, 0, stream>>>(
+    random_walk_kernel_impl<<<blocks(seed.size(0)), threads(), 0, stream>>>(
         rowptr_data, col_data, seed_data, rand_data, out_data, seed.size(0),
         walk_length);
   });
