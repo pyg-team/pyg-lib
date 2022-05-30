7f159430f89ee6e3a4ecba724929f0d174436274
rusty1s <matthias.fey@tu-dortmund.de>
2022-05-30 13:23:23 +0000

    update

--- pyg_lib/csrc/segment/cuda/matmul_kernel.cu
+++ pyg_lib/csrc/segment/cuda/matmul_kernel.cu
@@ -2,7 +2,13 @@
 #include <ATen/cuda/CUDAContext.h>
 #include <torch/library.h>
 
+#include <cutlass/cutlass.h>
+#include <cutlass/gemm/device/gemm.h>
+#include <cutlass/gemm/device/gemm_grouped.h>
+#include <cutlass/gemm/device/gemm_universal.h>
 #include <cutlass/gemm/gemm.h>
+#include <cutlass/gemm/kernel/default_gemm_grouped.h>
+#include <cutlass/gemm/kernel/gemm_grouped.h>
 
 namespace pyg {
 namespace segment {
@@ -13,6 +19,16 @@ at::Tensor matmul_kernel(const at::Tensor& input,
                          const at::Tensor& ptr,
                          const at::Tensor& other,
                          const at::Tensor& out) {
+  using ColumnMajor = cutlass::layout::ColumnMajor;
+
+  using CutlassGemm =
+      cutlass::gemm::device::Gemm<float,         // Data-type of A matrix
+                                  ColumnMajor,   // Layout of A matrix
+                                  float,         // Data-type of B matrix
+                                  ColumnMajor,   // Layout of B matrix
+                                  float,         // Data-type of C matrix
+                                  ColumnMajor>;  // Layout of C
+
   return out;
 }
 
