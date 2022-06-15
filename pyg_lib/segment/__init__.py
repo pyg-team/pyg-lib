ce60bcf914a7dff894e94d70ef06c3e55d52af94
rusty1s <matthias.fey@tu-dortmund.de>
2022-06-15 13:32:46 +0000

    update

---
+++ pyg_lib/segment/__init__.py
@@ -0,0 +1,66 @@
+from typing import List
+
+import torch
+from torch import Tensor
+
+
+def grouped_matmul(inputs: List[Tensor], others: List[Tensor]) -> List[Tensor]:
+    r"""Performs dense-dense matrix multiplication according to groups,
+    utilizing dedicated kernels that effectively parallelize over groups.
+
+    .. code-block:: python
+        inputs = [torch.randn(5, 16), torch.randn(3, 32)]
+        others = [torch.randn(16, 32), torch.randn(32, 64)]
+
+        outs = pyg_lib.segment.grouped_matmul(inputs, others)
+        assert len(outs) == 2
+        assert outs[0].size() == (5, 32)
+        assert outs[1].size() == (3, 64)
+
+    Args:
+        inputs (List[torch.Tensor]): List of left operand 2D matrices of shapes
+            :obj:`[N_i, K_i]`.
+        others (List[torch.Tensor]): List of right operand 2D matrices of
+            shapes :obj:`[K_i, M_i]`.
+
+    Returns:
+        List[torch.Tensor]: List of 2D output matrices of shapes
+            :obj:`[N_i, M_i]`.
+    """
+    return torch.ops.pyg.grouped_matmul(inputs, others)
+
+
+def segment_matmul(inputs: Tensor, ptr: Tensor, other: Tensor) -> Tensor:
+    r"""Performs dense-dense matrix multiplication according to segments along
+    the first dimension of :obj:`inputs` as given by :obj:`ptr`, utilizing
+    dedicated kernels that effectively parallelize over groups.
+
+    .. code-block:: python
+        inputs = torch.randn(8, 16)
+        ptr = torch.tensor([0, 5, 8])
+        other = torch.randn(2, 16, 32)
+
+        out = pyg_lib.segment.segment_matmul(inputs, ptr, other)
+        assert out.size() == (8, 32)
+        assert out[0:5] == inputs[0:5] @ other[0]
+        assert out[5:8] == inputs[5:8] @ other[1]
+
+    Args:
+        input (torch.Tensor): The left operand 2D matrix of shape
+            :obj:`[N, K]`.
+        ptr (torch.Tensor): Compressed vector of shape :obj:`[B + 1]`, holding
+            the boundaries of segments.
+            For best performance, given as a CPU tensor.
+        other (torch.Tensor): The right operand 3D tensor of shape
+            :obj:`[B, K, M]`.
+
+    Returns:
+        torch.Tensor: The 2D output matrix of shape :obj:`[N, M]`.
+    """
+    return torch.ops.pyg.segment_matmul(inputs, ptr, other)
+
+
+__all__ = [
+    'grouped_matmul',
+    'segment_matmul',
+]
