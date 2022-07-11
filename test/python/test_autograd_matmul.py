c69a4b864070945c6ec4c1a9713750ce82bc1c63
riship <riship@nvidia.com>
2022-07-11 12:18:25 -0700

    minor cleanups

--- test/python/test_autograd_matmul.py
+++ test/python/test_autograd_matmul.py
@@ -8,7 +8,7 @@ def test_grouped_matmul():
     # compute
     inputs = [torch.randn(5, 16), torch.randn(3, 32), torch.randn(4, 10)]
     others = [torch.randn(16, 32), torch.randn(32, 64), torch.randn(10, 20)]
-    outs = pyg_lib.segment.grouped_matmul(inputs, others)
+    outs = grouped_matmul(inputs, others)
     # check correctness
     assert len(outs) == 2
     assert outs[0].size() == (5, 32)
@@ -26,7 +26,7 @@ def test_segment_matmul():
     inputs = torch.randn(8, 16)
     ptr = torch.tensor([0, 5, 8])
     other = torch.randn(2, 16, 32)
-    out = pyg_lib.segment.segment_matmul(inputs, ptr, other)
+    out = segment_matmul(inputs, ptr, other)
     # check correctness
     assert out.size() == (8, 32)
     assert out[0:5] == inputs[0:5] @ other[0]
