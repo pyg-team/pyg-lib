de272be44b8e5e552ab780612829575491ee1255
riship <riship@nvidia.com>
2022-07-12 03:37:51 -0700

    minor fix

--- pyg_lib/csrc/ops/matmul.cpp
+++ pyg_lib/csrc/ops/matmul.cpp
@@ -85,8 +85,8 @@ class SegmentMatmul : public torch::autograd::Function<SegmentMatmul> {
     Variable input = saved[0];
     Variable ptr = saved[1];
     Variable other = saved[2].transpose(-2, -1).contiguous();
-    Variable grad_out = grad_outs[0] Variable other_grad =
-        segment_op.call(grad_out, ptr, other);
+    Variable grad_out = grad_outs[0];
+    Variable other_grad = segment_op.call(grad_out, ptr, other);
     if (torch::autograd::any_variable_requires_grad({input})) {
       input = input.transpose(-2, -1).contiguous();
       Variable input_grad = segment_op.call(input, ptr, grad_out);
