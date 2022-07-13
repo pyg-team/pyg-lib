de182100a95b0992f947a1a439a0f475d04fc672
riship <riship@nvidia.com>
2022-07-12 22:43:13 -0700

    trying to get backward for group matmul

--- pyg_lib/csrc/ops/matmul.cpp
+++ pyg_lib/csrc/ops/matmul.cpp
@@ -24,11 +24,7 @@ std::vector<at::Tensor> _grouped_matmul(const std::vector<at::Tensor>& input,
 
 std::vector<at::Tensor> concat(const std::vector<at::Tensor> t1,
                                const std::vector<at::Tensor> t2) {
-  for (size_t i = 0; i < t2.size(); ++i) {
-    Variable to_push = t2[i];
-    t1.push_back(to_push);
-  }
-  return t1;
+  t1.insert(t1.end(), t2.begin(), t2.end())
 }
 
 auto get_input(const std::vector<at::Tensor>& t, int split_index) {
