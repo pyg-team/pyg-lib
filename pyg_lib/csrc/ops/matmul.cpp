fb49cfe15502c1723aa7067bd751bfc0522a8ca3
Rishi Puri <puririshi98@berkeley.edu>
2022-07-13 05:04:55 -0700

    Autograd for `segment_matmul` (#61)
    * drafting
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    * drafting
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    * drafting
    
    * drafting
    
    * drafting
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    * drafting
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    * drafting
    
    * drafting
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    * addressed previous comments
    
    * minor cleanups
    
    * removing pytest to make pep8 happy
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    * backward part of the test
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    * minor fixups
    
    * minor fixups
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    * just segment matmul for now and i will ask internally for the solution to grouped
    
    * minor fix
    
    * minor fix
    
    * minor fix
    
    * minor fix
    
    * minor fix
    
    * minor fix
    
    * minor fix
    
    * minor fix
    
    * minor fix
    
    * minor fix
    
    * minor fix
    
    * minor fix
    
    * minor fix
    
    * minor fix
    
    * minor fix
    
    * minor fix
    
    * minor fix
    
    * minor fix
    
    * trying to get CI to pass...
    
    * trying to get CI to pass...
    
    * trying to get CI to pass...
    
    * trying to get CI to pass...
    
    * trying to get CI to pass...
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    * minor fix
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    * minor fix
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    * minor fix
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    * minor fix
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    * trying to get CI to pass...
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    * trying to get CI to pass...
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    * update
    
    * update
    
    * update
    
    * diff
    
    * update
    
    Co-authored-by: riship <riship@nvidia.com>
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>
    Co-authored-by: rusty1s <matthias.fey@tu-dortmund.de>

--- pyg_lib/csrc/ops/matmul.cpp
+++ pyg_lib/csrc/ops/matmul.cpp
@@ -2,15 +2,16 @@
 
 #include <ATen/core/dispatch/Dispatcher.h>
 #include <torch/library.h>
+#include <torch/script.h>
 
 namespace pyg {
 namespace ops {
 
-// Performs matrix multiplication across list of elements.
-std::vector<at::Tensor> grouped_matmul(const std::vector<at::Tensor>& input,
-                                       const std::vector<at::Tensor>& other) {
+namespace {
+
+std::vector<at::Tensor> _grouped_matmul(const std::vector<at::Tensor>& input,
+                                        const std::vector<at::Tensor>& other) {
   // TODO (matthias) Add TensorArg definitions.
-  // TODO (matthias) Add autograd support.
   // TODO (matthias) Add dispatcher support.
   static auto op = c10::Dispatcher::singleton()
                        .findSchemaOrThrow("pyg::grouped_matmul", "")
@@ -18,16 +19,65 @@ std::vector<at::Tensor> grouped_matmul(const std::vector<at::Tensor>& input,
   return op.call(input, other);
 }
 
-// Performs matrix multiplication according to segments.
-at::Tensor segment_matmul(const at::Tensor& input,
-                          const at::Tensor& ptr,
-                          const at::Tensor& other) {
+at::Tensor _segment_matmul(const at::Tensor& input,
+                           const at::Tensor& ptr,
+                           const at::Tensor& other) {
   // TODO (matthias) Add TensorArg definitions.
-  // TODO (matthias) Add autograd support.
   static auto op = c10::Dispatcher::singleton()
                        .findSchemaOrThrow("pyg::segment_matmul", "")
                        .typed<decltype(segment_matmul)>();
   return op.call(input, ptr, other);
+}
+
+using torch::autograd::AutogradContext;
+using torch::autograd::Variable;
+using torch::autograd::variable_list;
+
+// Performs matrix multiplication according to segments.
+class SegmentMatmul : public torch::autograd::Function<SegmentMatmul> {
+ public:
+  static variable_list forward(AutogradContext* ctx,
+                               Variable input,
+                               Variable ptr,
+                               Variable other) {
+    Variable out = _segment_matmul(input, ptr, other);
+    ctx->save_for_backward({input, ptr, other});
+    return {out};
+  }
+
+  static variable_list backward(AutogradContext* ctx, variable_list grad_outs) {
+    auto grad_out = grad_outs[0];
+    auto saved = ctx->get_saved_variables();
+    auto input = saved[0], ptr = saved[1], other = saved[2];
+
+    auto input_grad = Variable(), other_grad = Variable();
+    if (torch::autograd::any_variable_requires_grad({input})) {
+      // TODO (matthias) get rid of unnecessary `contiguous` here.
+      auto other_t = other.transpose(-2, -1).contiguous();
+      input_grad = _segment_matmul(grad_out, ptr, other_t);
+    }
+    if (torch::autograd::any_variable_requires_grad({other})) {
+      // TODO (matthias) implement backward pass for `other`.
+    }
+
+    return {input_grad, Variable(), other_grad};
+  }
+};
+
+}  // namespace
+
+// Performs matrix multiplication across list of elements.
+std::vector<at::Tensor> grouped_matmul(const std::vector<at::Tensor>& input,
+                                       const std::vector<at::Tensor>& other) {
+  // TODO (matthias) Add autograd support.
+  return _grouped_matmul(input, other);
+}
+
+// Performs matrix multiplication according to segments.
+at::Tensor segment_matmul(const at::Tensor& input,
+                          const at::Tensor& ptr,
+                          const at::Tensor& other) {
+  return SegmentMatmul::apply(input, ptr, other)[0];
 }
 
 TORCH_LIBRARY_FRAGMENT(pyg, m) {
