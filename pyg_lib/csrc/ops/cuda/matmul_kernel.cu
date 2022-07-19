1121514107635a06ec2725ff06a584664db23743
riship <riship@nvidia.com>
2022-07-19 16:26:59 -0700

    drafting

--- pyg_lib/csrc/ops/cuda/matmul_kernel.cu
+++ pyg_lib/csrc/ops/cuda/matmul_kernel.cu
@@ -50,7 +50,7 @@ void grouped_matmul_out_kernel(const std::vector<at::Tensor>& input,
           float, 8, float, float>,                   //
       cutlass::gemm::threadblock::                   // Swizzling Operator
       GemmIdentityThreadblockSwizzle<8>,             //
-      2,                                             // Stages
+      3,                                             // Stages
       cutlass::arch::OpMultiplyAdd                   // Operation
       >::GemmKernel;
 
