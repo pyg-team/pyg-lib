0a6eee3badf3d7cf337af58e6e330c7e6af6e8ae
riship <riship@nvidia.com>
2022-07-21 15:42:05 -0700

    trying to fix backward

--- pyg_lib/csrc/ops/matmul.cpp
+++ pyg_lib/csrc/ops/matmul.cpp
@@ -16,6 +16,7 @@ std::vector<at::Tensor> _grouped_matmul(const std::vector<at::Tensor>& input,
   static auto op = c10::Dispatcher::singleton()
                        .findSchemaOrThrow("pyg::grouped_matmul_kern", "")
                        .typed<decltype(_grouped_matmul)>();
+  ASSERT_TRUE(op.has_value());
   std::cout << "================= DEBUG =================" << std::endl;
   std::cout << "about to call op.call";
   return op.call(input, other);
