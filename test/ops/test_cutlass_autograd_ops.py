286c862e59758b318f05704209ef3c3677d2e150
riship <riship@nvidia.com>
2022-07-22 19:00:55 -0700

    autograd tests

--- test/ops/test_cutlass_autograd_ops.py
+++ test/ops/test_cutlass_autograd_ops.py
@@ -3,19 +3,23 @@ import torch
 import pyg_lib
 
 
+def assert_close_enough(x, y, tol=6e-3)
+    # TODO Rishi: Work w/ Cutlass to lower the high error (as large as ~5e-3)
+    assert ((x - y).abs().max() <= tol), 'Max Abs Err: ' + str((x-y).abs().max()) + ', Tolerace: ' + str(tol)
+
+
 def test_segment_matmul_autograd():
     inputs = torch.randn((8, 16), requires_grad=True, device='cuda:0')
     ptr = torch.tensor([0, 5, 8]).cuda()
     other = torch.randn((2, 16, 32), requires_grad=True, device='cuda:0')
     out = pyg_lib.ops.segment_matmul(inputs, ptr, other)
     assert out.size() == (8, 32)
+    assert_close_enough(out[0:5], inputs[0:5] @ other[0])
+    assert_close_enough(out[5:8], inputs[5:8] @ other[1])
     out.sum().backward()
     assert other.grad.shape == other.shape
     assert inputs.grad.shape == inputs.shape
-    assert (out[0:5] == inputs[0:5] @ other[0]).all(), 'Abs Err: ' + str(
-        (inputs[0:5] @ other[0] - out[0:5]).abs())
-    assert (out[5:8] == inputs[5:8] @ other[1]).all(), 'Abs Err: ' + str(
-        (inputs[5:8] @ other[1] - out[5:8]).abs())
+
     print('test_segment_matmul_autograd passed!')
 
 
@@ -29,13 +33,11 @@ def test_grouped_matmul_autograd():
     assert len(outs) == 2
     assert outs[0].size() == (5, 32)
     assert outs[1].size() == (3, 64)
+    assert_close_enough(outs[0], inputs[0] @ others[0])
+    assert_close_enough(outs[1], inputs[1] @ others[1])
     (outs[0].sum() + outs[1].sum()).backward()
     assert outs[0].grad.size() == (5, 32)
     assert outs[1].grad.size() == (3, 64)
-    assert (outs[0] == inputs[0] @ others[0]).all(), 'Abs Err: ' + str(
-        (inputs[0] @ others[0] - outs[0]).abs())
-    assert (outs[1] == inputs[1] @ others[1]).all(), 'Abs Err: ' + str(
-        (inputs[1] @ others[1] - outs[1]).abs())
     print('test_grouped_matmul_autograd passed!')
 
 
