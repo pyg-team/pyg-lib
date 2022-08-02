97cb19dfdb890dd85fcc745cb402fa182723e3b2
riship <riship@nvidia.com>
2022-08-02 04:53:40 -0700

    working on padder

--- pyg_lib/csrc/ops/cuda/matmul_kernel.cu
+++ pyg_lib/csrc/ops/cuda/matmul_kernel.cu
@@ -17,9 +17,7 @@ namespace F = torch::nn::functional;
 at::Tensor pad_to_align(const at::Tensor& input) {
   int dim_0_pad = (((input.size(0) / 4) + 1) * 4) - input.size(0);
   int dim_1_pad = (((input.size(1) / 4) + 1) * 4) - input.size(1);
-  return F::pad(
-      input,
-      F::PadFuncOptions({0, dim_1_pad, 0, dim_0_pad}).mode(torch::kConstant));
+  return F::pad(input, F::PadFuncOptions({0, dim_1_pad, 0, dim_0_pad}).mode(torch::kConstant));
 }
 
 void grouped_matmul_out_kernel(const std::vector<at::Tensor>& input,
@@ -32,12 +30,12 @@ void grouped_matmul_out_kernel(const std::vector<at::Tensor>& input,
   // TODO (matthias) Allow for other types than `float`.
   // TODO (matthias) Are these attributes correctly set?
   using GemmKernel = typename cutlass::gemm::kernel::DefaultGemmGrouped<
-      float,                             // Element A
-      cutlass::layout::RowMajor,         // Layout A
-      cutlass::ComplexTransform::kNone,  //
-      4,                          // Granularity A (4 is the max for 32 bit)
-      float,                      // Element B
-      cutlass::layout::RowMajor,  // Layout B
+      float,                                         // Element A
+      cutlass::layout::RowMajor,                     // Layout A
+      cutlass::ComplexTransform::kNone,              //
+      4,                                             // Granularity A (4 is the max for 32 bit)
+      float,                                         // Element B
+      cutlass::layout::RowMajor,                     // Layout B
       cutlass::ComplexTransform::kNone,              //
       4,                                             // Granularity B
       float,                                         // Element C&D
@@ -76,91 +74,93 @@ void grouped_matmul_out_kernel(const std::vector<at::Tensor>& input,
     } else {
       ptr_C_host[i] = out[i].data_ptr<float>();
     }
+  } 
 
-    cutlass::DeviceAllocation<float*> ptr_A;
-    ptr_A.reset(num_matrices);
-    ptr_A.copy_from_host(ptr_A_host.data());
+  cutlass::DeviceAllocation<float*> ptr_A;
+  ptr_A.reset(num_matrices);
+  ptr_A.copy_from_host(ptr_A_host.data());
 
-    cutlass::DeviceAllocation<float*> ptr_B;
-    ptr_B.reset(num_matrices);
-    ptr_B.copy_from_host(ptr_B_host.data());
+  cutlass::DeviceAllocation<float*> ptr_B;
+  ptr_B.reset(num_matrices);
+  ptr_B.copy_from_host(ptr_B_host.data());
 
-    cutlass::DeviceAllocation<float*> ptr_C;
-    ptr_C.reset(num_matrices);
-    ptr_C.copy_from_host(ptr_C_host.data());
+  cutlass::DeviceAllocation<float*> ptr_C;
+  ptr_C.reset(num_matrices);
+  ptr_C.copy_from_host(ptr_C_host.data());
 
-    std::vector<cutlass::gemm::GemmCoord> all_problems(num_matrices);
-    std::vector<int64_t> ld_A_host(num_matrices);
-    std::vector<int64_t> ld_B_host(num_matrices);
-    std::vector<int64_t> ld_C_host(num_matrices);
+  std::vector<cutlass::gemm::GemmCoord> all_problems(num_matrices);
+  std::vector<int64_t> ld_A_host(num_matrices);
+  std::vector<int64_t> ld_B_host(num_matrices);
+  std::vector<int64_t> ld_C_host(num_matrices);
 
-    for (size_t i = 0; i < num_matrices; ++i) {
-      auto m = input[i].size(0), k = input[i].size(1), n = out[i].size(1);
-      TORCH_CHECK(input[i].size(-1) == other[i].size(-2), "Shape mismatch");
-      all_problems[i] = cutlass::gemm::GemmCoord(m, n, k);
-      ld_A_host[i] = GemmKernel::LayoutA::packed({m, k}).stride(0);
-      ld_B_host[i] = GemmKernel::LayoutB::packed({k, n}).stride(0);
-      ld_C_host[i] = GemmKernel::LayoutC::packed({m, n}).stride(0);
-    }
-
-    cutlass::DeviceAllocation<cutlass::gemm::GemmCoord> all_problems_device;
-    all_problems_device.reset(num_matrices);
-    all_problems_device.copy_from_host(all_problems.data());
-
-    cutlass::DeviceAllocation<int64_t> ld_A;
-    ld_A.reset(num_matrices);
-    ld_A.copy_from_host(ld_A_host.data());
-
-    cutlass::DeviceAllocation<int64_t> ld_B;
-    ld_B.reset(num_matrices);
-    ld_B.copy_from_host(ld_B_host.data());
-
-    cutlass::DeviceAllocation<int64_t> ld_C;
-    ld_C.reset(num_matrices);
-    ld_C.copy_from_host(ld_C_host.data());
-
-    using EpilogueOutputOp = typename GemmKernel::Epilogue::OutputOp;
-    typename EpilogueOutputOp::Params epilogue_op(1.0, 0.0);
-
-    using GemmGrouped = cutlass::gemm::device::GemmGrouped<GemmKernel>;
-    typename GemmGrouped::Arguments args(
-        all_problems_device.get(), num_matrices, /*threadblock_count=*/1024,
-        epilogue_op, ptr_A.get(), ptr_B.get(), ptr_C.get(), ptr_C.get(),
-        ld_A.get(), ld_B.get(), ld_C.get(), ld_C.get());
-
-    GemmGrouped gemm;
-    auto status = gemm.initialize(args);
-    TORCH_CHECK(status == cutlass::Status::kSuccess, "GroupedGEMM init failed");
-    status = gemm.run();
-    TORCH_CHECK(status == cutlass::Status::kSuccess, "GroupedGEMM run failed");
+  for (size_t i = 0; i < num_matrices; ++i) {
+    auto m = input[i].size(0), k = input[i].size(1), n = out[i].size(1);
+    TORCH_CHECK(input[i].size(-1) == other[i].size(-2), "Shape mismatch");
+    all_problems[i] = cutlass::gemm::GemmCoord(m, n, k);
+    ld_A_host[i] = GemmKernel::LayoutA::packed({m, k}).stride(0);
+    ld_B_host[i] = GemmKernel::LayoutB::packed({k, n}).stride(0);
+    ld_C_host[i] = GemmKernel::LayoutC::packed({m, n}).stride(0);
   }
 
-  std::vector<at::Tensor> grouped_matmul_kernel(
-      const std::vector<at::Tensor>& input,
-      const std::vector<at::Tensor>& other) {
-    std::vector<at::Tensor> out(input.size());
-    for (size_t i = 0; i < input.size(); ++i)
-      out[i] = input[i].new_empty({input[i].size(0), other[i].size(-1)});
-    grouped_matmul_out_kernel(input, other, out);
+  cutlass::DeviceAllocation<cutlass::gemm::GemmCoord> all_problems_device;
+  all_problems_device.reset(num_matrices);
+  all_problems_device.copy_from_host(all_problems.data());
 
-    return out;
-  }
+  cutlass::DeviceAllocation<int64_t> ld_A;
+  ld_A.reset(num_matrices);
+  ld_A.copy_from_host(ld_A_host.data());
 
-  at::Tensor segment_matmul_kernel(
-      const at::Tensor& input, const at::Tensor& ptr, const at::Tensor& other) {
-    const auto size = pyg::utils::size_from_ptr(ptr).cpu();
-    // TODO (matthias) Allow for other types than `int64_t`.
-    const auto sizes = at::IntArrayRef(size.data_ptr<int64_t>(), size.numel());
-    const auto out = input.new_empty({input.size(0), other.size(-1)});
+  cutlass::DeviceAllocation<int64_t> ld_B;
+  ld_B.reset(num_matrices);
+  ld_B.copy_from_host(ld_B_host.data());
 
-    // TODO (matthias) Better handle non-contiguous memory layouts.
-    grouped_matmul_out_kernel(
-        input.contiguous().split_with_sizes(/*split_size=*/sizes, /*dim=*/0),
-        other.contiguous().split(/*split_size=*/1, /*dim=*/0),
-        out.split_with_sizes(/*split_size=*/sizes, /*dim=*/0));
+  cutlass::DeviceAllocation<int64_t> ld_C;
+  ld_C.reset(num_matrices);
+  ld_C.copy_from_host(ld_C_host.data());
 
-    return out;
-  }
+  using EpilogueOutputOp = typename GemmKernel::Epilogue::OutputOp;
+  typename EpilogueOutputOp::Params epilogue_op(1.0, 0.0);
+
+  using GemmGrouped = cutlass::gemm::device::GemmGrouped<GemmKernel>;
+  typename GemmGrouped::Arguments args(
+      all_problems_device.get(), num_matrices, /*threadblock_count=*/1024,
+      epilogue_op, ptr_A.get(), ptr_B.get(), ptr_C.get(), ptr_C.get(),
+      ld_A.get(), ld_B.get(), ld_C.get(), ld_C.get());
+
+  GemmGrouped gemm;
+  auto status = gemm.initialize(args);
+  TORCH_CHECK(status == cutlass::Status::kSuccess, "GroupedGEMM init failed");
+  status = gemm.run();
+  TORCH_CHECK(status == cutlass::Status::kSuccess, "GroupedGEMM run failed");
+}
+
+std::vector<at::Tensor> grouped_matmul_kernel(
+    const std::vector<at::Tensor>& input,
+    const std::vector<at::Tensor>& other) {
+  std::vector<at::Tensor> out(input.size());
+  for (size_t i = 0; i < input.size(); ++i)
+    out[i] = input[i].new_empty({input[i].size(0), other[i].size(-1)});
+  grouped_matmul_out_kernel(input, other, out);
+
+  return out;
+}
+
+at::Tensor segment_matmul_kernel(const at::Tensor& input,
+                                 const at::Tensor& ptr,
+                                 const at::Tensor& other) {
+  const auto size = pyg::utils::size_from_ptr(ptr).cpu();
+  // TODO (matthias) Allow for other types than `int64_t`.
+  const auto sizes = at::IntArrayRef(size.data_ptr<int64_t>(), size.numel());
+  const auto out = input.new_empty({input.size(0), other.size(-1)});
+
+  // TODO (matthias) Better handle non-contiguous memory layouts.
+  grouped_matmul_out_kernel(
+      input.contiguous().split_with_sizes(/*split_size=*/sizes, /*dim=*/0),
+      other.contiguous().split(/*split_size=*/1, /*dim=*/0),
+      out.split_with_sizes(/*split_size=*/sizes, /*dim=*/0));
+
+  return out;
+}
 
 }  // namespace
 
