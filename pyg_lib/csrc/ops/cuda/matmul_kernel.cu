63c1d2c57e6eab46716f2965e86e608af400b5cc
riship <riship@nvidia.com>
2022-08-02 06:51:14 -0700

    working on padder

--- pyg_lib/csrc/ops/cuda/matmul_kernel.cu
+++ pyg_lib/csrc/ops/cuda/matmul_kernel.cu
@@ -99,28 +99,10 @@ void grouped_matmul_out_kernel(const std::vector<at::Tensor>& input,
   std::vector<int64_t> ld_B_host(num_matrices);
   std::vector<int64_t> ld_C_host(num_matrices);
   for (size_t i = 0; i < num_matrices; ++i) {
-    std::cout << "================= input =================" << std::endl;
-    std::cout << input[i].size(0) << std::endl;
-    std::cout << input[i].size(1) << std::endl;
-    std::cout << "================= other =================" << std::endl;
-    std::cout << other[i].size(0) << std::endl;
-    std::cout << other[i].size(1) << std::endl;
-  }
-  std::cout << "================= After Pad =================" << std::endl;
-
-  for (size_t i = 0; i < num_matrices; ++i) {
-    std::cout << "================= input =================" << std::endl;
-    std::cout << new_input[i].size(0) << std::endl;
-    std::cout << new_input[i].size(1) << std::endl;
-    std::cout << "================= other =================" << std::endl;
-    std::cout << new_other[i].size(0) << std::endl;
-    std::cout << new_other[i].size(1) << std::endl;
-  }
-  for (size_t i = 0; i < num_matrices; ++i) {
     auto m = new_input[i].size(0), k = new_other[i].size(1),
          n = new_out[i].size(1);
 
-    TORCH_CHECK(new_input[i].size(1) == new_other[i].size(0), "Shape mismatch");
+    TORCH_CHECK(new_input[i].size(-1) == new_other[i].size(-2), "Shape mismatch");
     all_problems[i] = cutlass::gemm::GemmCoord(m, n, k);
     ld_A_host[i] = GemmKernel::LayoutA::packed({m, k}).stride(0);
     ld_B_host[i] = GemmKernel::LayoutB::packed({k, n}).stride(0);
