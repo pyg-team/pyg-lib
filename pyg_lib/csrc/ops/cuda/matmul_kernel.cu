82df60db0b0a4b3e3a49dcfa7b448c23b1fcd094
riship <riship@nvidia.com>
2022-08-02 06:28:21 -0700

    working on padder

--- pyg_lib/csrc/ops/cuda/matmul_kernel.cu
+++ pyg_lib/csrc/ops/cuda/matmul_kernel.cu
@@ -105,7 +105,7 @@ void grouped_matmul_out_kernel(const std::vector<at::Tensor>& input,
     std::cout << "================= DEBUG =================" << std::endl;
     std::cout << new_input[i].size(1) << std::endl;
     std::cout << "================= DEBUG =================" << std::endl;
-    std::cout << new_input[i].size(0) << std::endl;
+    std::cout << new_other[i].size(0) << std::endl;
     TORCH_CHECK(new_input[i].size(1) == new_other[i].size(0), "Shape mismatch");
     all_problems[i] = cutlass::gemm::GemmCoord(m, n, k);
     ld_A_host[i] = GemmKernel::LayoutA::packed({m, k}).stride(0);
