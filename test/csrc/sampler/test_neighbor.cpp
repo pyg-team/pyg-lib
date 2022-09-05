c5f34881e44b14ef21618a118efe0f25cd0b7eab
Matthias Fey <matthias.fey@tu-dortmund.de>
2022-09-05 23:08:00 +0200

    Add 'disjoint` neighbor sampling test (#89)
    * add test
    
    * changelog

--- test/csrc/sampler/test_neighbor.cpp
+++ test/csrc/sampler/test_neighbor.cpp
@@ -8,19 +8,43 @@ TEST(NeighborTest, BasicAssertions) {
   auto options = at::TensorOptions().dtype(at::kLong);
 
   auto graph = cycle_graph(/*num_nodes=*/6, options);
-  auto seed = at::arange(2, 3, options);
+  auto seed = at::arange(2, 4, options);
   std::vector<int64_t> num_neighbors = {2, 2};
 
   auto out = pyg::sampler::neighbor_sample(/*rowptr=*/std::get<0>(graph),
                                            /*col=*/std::get<1>(graph), seed,
                                            num_neighbors);
 
-  auto expected_row = at::tensor({0, 0, 1, 1, 2, 2}, options);
+  auto expected_row = at::tensor({0, 0, 1, 1, 2, 2, 3, 3}, options);
   EXPECT_TRUE(at::equal(std::get<0>(out), expected_row));
-  auto expected_col = at::tensor({1, 2, 3, 0, 0, 4}, options);
+  auto expected_col = at::tensor({2, 1, 0, 3, 4, 0, 1, 5}, options);
   EXPECT_TRUE(at::equal(std::get<1>(out), expected_col));
-  auto expected_nodes = at::tensor({2, 1, 3, 0, 4}, options);
+  auto expected_nodes = at::tensor({2, 3, 1, 4, 0, 5}, options);
   EXPECT_TRUE(at::equal(std::get<2>(out), expected_nodes));
-  auto expected_edges = at::tensor({4, 5, 2, 3, 6, 7}, options);
+  auto expected_edges = at::tensor({4, 5, 6, 7, 2, 3, 8, 9}, options);
+  EXPECT_TRUE(at::equal(std::get<3>(out).value(), expected_edges));
+}
+
+TEST(NeighborDisjointTest, BasicAssertions) {
+  auto options = at::TensorOptions().dtype(at::kLong);
+
+  auto graph = cycle_graph(/*num_nodes=*/6, options);
+  auto seed = at::arange(2, 4, options);
+  std::vector<int64_t> num_neighbors = {2, 2};
+
+  auto out = pyg::sampler::neighbor_sample(
+      /*rowptr=*/std::get<0>(graph),
+      /*col=*/std::get<1>(graph), seed, num_neighbors, /*time=*/c10::nullopt,
+      /*replace=*/false, /*directed=*/true, /*disjoint=*/true);
+
+  auto expected_row = at::tensor({0, 0, 1, 1, 2, 2, 3, 3, 4, 4, 5, 5}, options);
+  EXPECT_TRUE(at::equal(std::get<0>(out), expected_row));
+  auto expected_col = at::tensor({2, 3, 4, 5, 6, 0, 0, 7, 8, 1, 1, 9}, options);
+  EXPECT_TRUE(at::equal(std::get<1>(out), expected_col));
+  auto expected_nodes = at::tensor(
+      {0, 2, 1, 3, 0, 1, 0, 3, 1, 2, 1, 4, 0, 0, 0, 4, 1, 1, 1, 5}, options);
+  EXPECT_TRUE(at::equal(std::get<2>(out), expected_nodes.view({10, 2})));
+  auto expected_edges =
+      at::tensor({4, 5, 6, 7, 2, 3, 6, 7, 4, 5, 8, 9}, options);
   EXPECT_TRUE(at::equal(std::get<3>(out).value(), expected_edges));
 }
