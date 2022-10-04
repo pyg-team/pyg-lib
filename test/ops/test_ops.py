b31a2e7f5f5b6cfeeb90107928a65cefe1cb7dc6
riship <riship@nvidia.com>
2022-10-04 14:38:41 -0700

    trying to go back to cpp autograd

--- test/ops/test_ops.py
+++ test/ops/test_ops.py
@@ -3,7 +3,7 @@ import torch
 
 import pyg_lib
 
-#DEVICE_STRS = ['cpu']
+#DEVICE_STRS = ['cpu'] # cpu has enormous errors or segfaults, see Issue 119
 if torch.cuda.is_available():
     DEVICE_STRS.append('cuda:0')
 
