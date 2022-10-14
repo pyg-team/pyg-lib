451e3c0f6954139c28247096c6a0fdf71594de5e
rusty1s <matthias.fey@tu-dortmund.de>
2022-10-14 08:18:27 +0000

    reset

---
+++ pyg_lib/csrc/sampler/cpu/neighbor_kernel.h
@@ -0,0 +1,43 @@
+#include <ATen/ATen.h>
+#include <torch/library.h>
+#include "pyg_lib/csrc/utils/types.h"
+
+namespace pyg {
+namespace sampler {
+
+std::tuple<at::Tensor, at::Tensor, at::Tensor, c10::optional<at::Tensor>>
+neighbor_sample_kernel(const at::Tensor& rowptr,
+                       const at::Tensor& col,
+                       const at::Tensor& seed,
+                       const std::vector<int64_t>& num_neighbors,
+                       const c10::optional<at::Tensor>& time,
+                       const c10::optional<at::Tensor>& seed_time,
+                       bool csc,
+                       bool replace,
+                       bool directed,
+                       bool disjoint,
+                       std::string temporal_strategy,
+                       bool return_edge_id);
+
+std::tuple<c10::Dict<rel_type, at::Tensor>,
+           c10::Dict<rel_type, at::Tensor>,
+           c10::Dict<node_type, at::Tensor>,
+           c10::optional<c10::Dict<rel_type, at::Tensor>>>
+hetero_neighbor_sample_kernel(
+    const std::vector<node_type>& node_types,
+    const std::vector<edge_type>& edge_types,
+    const c10::Dict<rel_type, at::Tensor>& rowptr_dict,
+    const c10::Dict<rel_type, at::Tensor>& col_dict,
+    const c10::Dict<node_type, at::Tensor>& seed_dict,
+    const c10::Dict<rel_type, std::vector<int64_t>>& num_neighbors_dict,
+    const c10::optional<c10::Dict<node_type, at::Tensor>>& time_dict,
+    const c10::optional<c10::Dict<node_type, at::Tensor>>& seed_time_dict,
+    bool csc,
+    bool replace,
+    bool directed,
+    bool disjoint,
+    std::string temporal_strategy,
+    bool return_edge_id);
+
+}  // namespace sampler
+}  // namespace pyg
