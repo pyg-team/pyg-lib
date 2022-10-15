06b45e338fa9c53e944b756f635372734c7c2c9e
kgajdamo <kinga.gajdamowicz@intel.com>
2022-10-15 09:26:51 +0200

    Add `seed_time_dict` argument to `hetero_sample` benchmark (#126)

--- benchmark/sampler/hetero_neighbor.py
+++ benchmark/sampler/hetero_neighbor.py
@@ -55,6 +55,7 @@ def test_hetero_neighbor(dataset, **kwargs):
                     seed_dict,
                     num_neighbors_dict,
                     None,  # time_dict
+                    None,  # seed_time_dict
                     True,  # csc
                     False,  # replace
                     True,  # directed
