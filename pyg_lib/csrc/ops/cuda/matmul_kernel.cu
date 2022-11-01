6a76a9a9eb9d08110052f7a4873f761448051f14
riship <riship@nvidia.com>
2022-11-01 10:32:07 -0700

    moving to cpp land

--- pyg_lib/csrc/ops/cuda/matmul_kernel.cu
+++ pyg_lib/csrc/ops/cuda/matmul_kernel.cu
@@ -314,11 +314,9 @@ at::Tensor segment_matmul_kernel(const at::Tensor& input,
 #if TORCH_VERSION_MINOR >= 14 or TORCH_VERSION_MAJOR > 1
   auto input_nested =
       torch::nested::as_nested_tensor(
-          input.contiguous().split_with_sizes(/*split_size=*/sizes, /*dim=*/0))
-          .contiguous();
+          input.contiguous().split_with_sizes(/*split_size=*/sizes, /*dim=*/0));
   auto other_nested = torch::nested::as_nested_tensor(
-                          other.contiguous().split(/*split_size=*/1, /*dim=*/0))
-                          .contiguous();
+                          other.contiguous().split(/*split_size=*/1, /*dim=*/0));
   auto out = torch::cat(
       at::native::bmm_nested_cuda(input_nested, other_nested).contiguous().unbind(),
       0);
