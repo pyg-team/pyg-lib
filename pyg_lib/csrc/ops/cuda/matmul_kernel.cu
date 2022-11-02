076f6571d6953d59a0e1dd68a719de6e393b80c3
riship <riship@nvidia.com>
2022-11-02 05:23:02 -0700

    moving to cpp land

--- pyg_lib/csrc/ops/cuda/matmul_kernel.cu
+++ pyg_lib/csrc/ops/cuda/matmul_kernel.cu
@@ -311,9 +311,9 @@ at::Tensor segment_matmul_kernel(const at::Tensor& input,
   // TODO (matthias) Allow for other types than `int64_t`.
   const auto sizes = at::IntArrayRef(size.data_ptr<int64_t>(), size.numel());
 #if TORCH_VERSION_MINOR >= 14 or TORCH_VERSION_MAJOR > 1
-  auto input_nested = at::nested::nested_tensor(
+  auto input_nested = aten::nested_tensor(
       input.contiguous().split_with_sizes(/*split_size=*/sizes, /*dim=*/0));
-  auto other_nested = at::nested::nested_tensor(
+  auto other_nested = aten::nested_tensor(
                           other.contiguous().split(/*split_size=*/1, /*dim=*/0))
                           .squeeze(1);
   auto out_nested = at::native::bmm_nested_cuda(input_nested, other_nested);
