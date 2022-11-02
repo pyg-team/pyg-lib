49036a279bf572fe58864c8e6cf3ab14e2e0b906
rusty1s <matthias.fey@tu-dortmund.de>
2022-11-02 16:23:02 +0000

    update

--- pyg_lib/ops/scatter_reduce.py
+++ pyg_lib/ops/scatter_reduce.py
@@ -5,4 +5,15 @@ from torch import Tensor
 
 def fused_scatter_reduce(inputs: Tensor, index: Tensor, dim_size: int,
                          reduce: List[str]) -> Tensor:
-    pass
+    # TODO (matthias): Add support for `out`.
+    # TODO (matthias): Add backward functionality.
+    # TODO (matthias): Add support for inputs.dim() != 2.
+    # TODO (matthias): Add support for index.dim() != 1.
+    # TODO (matthias) Add support for `dim` argument.
+    assert len(reduce) > 1
+    assert inputs.is_cuda and index.is_cuda
+    assert inputs.dim() == 2 and index.dim() == 1
+
+    out = inputs.new(dim_size, len(reduce) * inputs.size(1))
+
+    return out
--- test/ops/test_scatter_reduce.py
+++ test/ops/test_scatter_reduce.py
@@ -1,8 +1,14 @@
-# from pyg_lib import fused_scatter_reduce
+import torch
+
+from pyg_lib.ops import fused_scatter_reduce
 from pyg_lib.testing import onlyCUDA, onlyTriton
 
 
 @onlyCUDA
 @onlyTriton
 def test_fused_scatter_reduce():
-    pass
+    x = torch.randn(5, 4, device='cuda')
+    index = torch.tensor([0, 1, 0, 1, 0], device='cuda')
+
+    out = fused_scatter_reduce(x, index, dim_size=2, reduce=['sum', 'max'])
+    assert out.size() == (2, 8)
