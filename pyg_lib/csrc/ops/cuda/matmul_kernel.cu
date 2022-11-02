1586e9a2e27a8ddec37279cadeffd072d695346b
riship <riship@nvidia.com>
2022-11-02 05:20:00 -0700

    moving to cpp land

--- pyg_lib/csrc/ops/cuda/matmul_kernel.cu
+++ pyg_lib/csrc/ops/cuda/matmul_kernel.cu
@@ -311,13 +311,13 @@ at::Tensor segment_matmul_kernel(const at::Tensor& input,
   // TODO (matthias) Allow for other types than `int64_t`.
   const auto sizes = at::IntArrayRef(size.data_ptr<int64_t>(), size.numel());
 #if TORCH_VERSION_MINOR >= 14 or TORCH_VERSION_MAJOR > 1
-  auto input_nested = at::native::nested_tensor(
+  auto input_nested = at::nested::nested_tensor(
       input.contiguous().split_with_sizes(/*split_size=*/sizes, /*dim=*/0));
-  auto other_nested = at::native::nested_tensor(
+  auto other_nested = at::nested::nested_tensor(
                           other.contiguous().split(/*split_size=*/1, /*dim=*/0))
                           .squeeze(1);
   auto out_nested = at::native::bmm_nested_cuda(input_nested, other_nested);
-  auto out = at::native::cat(out_nested.contiguous().unbind());
+  auto out = at::cat(out_nested.contiguous().unbind());
 #else
   const auto out = input.new_empty({input.size(0), other.size(-1)});
   // TODO (matthias) Better handle non-contiguous memory layouts.
