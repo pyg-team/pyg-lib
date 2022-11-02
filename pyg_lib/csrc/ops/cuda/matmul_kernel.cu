5edfb6860a0f51f141b95adce8c29df612a80b88
riship <riship@nvidia.com>
2022-11-02 04:56:54 -0700

    moving to cpp land

--- pyg_lib/csrc/ops/cuda/matmul_kernel.cu
+++ pyg_lib/csrc/ops/cuda/matmul_kernel.cu
@@ -317,7 +317,7 @@ at::Tensor segment_matmul_kernel(const at::Tensor& input,
                           other.contiguous().split(/*split_size=*/1, /*dim=*/0))
                           .squeeze(1);
   auto out_nested = at::native::bmm_nested_cuda(input_nested, other_nested);
-  auto out = torch::cat(out_nested.contiguous().unbind(), 0);
+  auto out = at::native::cat(out_nested.contiguous().unbind(), 0);
 #else
   const auto out = input.new_empty({input.size(0), other.size(-1)});
   // TODO (matthias) Better handle non-contiguous memory layouts.
