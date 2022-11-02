756043574754e287b89a5d28d18b14ec4fe5fb05
riship <riship@nvidia.com>
2022-11-02 04:48:29 -0700

    moving to cpp land

--- pyg_lib/csrc/ops/cuda/matmul_kernel.cu
+++ pyg_lib/csrc/ops/cuda/matmul_kernel.cu
@@ -3,7 +3,7 @@
 #include <ATen/cuda/CUDAContext.h>
 #include <cutlass/util/host_tensor.h>
 #include <torch/library.h>
-#include <torch/torch.h>
+#include <torch/version.h>
 #include "cutlass/cutlass.h"
 #include "cutlass/gemm/device/gemm_grouped.h"
 #include "cutlass/gemm/device/gemm_universal.h"
