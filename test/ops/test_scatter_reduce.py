1d07625970b231f8574967f009760596299a1836
rusty1s <matthias.fey@tu-dortmund.de>
2022-11-03 15:47:56 +0000

    update

--- test/ops/test_scatter_reduce.py
+++ test/ops/test_scatter_reduce.py
@@ -18,3 +18,38 @@ def test_fused_scatter_reduce():
     assert torch.allclose(out[1, 0:4], x[index == 1].sum(dim=0))
     assert torch.allclose(out[0, 4:8], x[index == 0].mean(dim=0))
     assert torch.allclose(out[1, 4:8], x[index == 1].mean(dim=0))
+
+
+if __name__ == '__main__':  # Benchmarking
+    import time
+
+    import torch_scatter
+
+    x = torch.randn(50000, 64, device='cuda')
+    index = torch.randint(1000, (x.size(0), ), device='cuda')
+
+    num_warmups = 1000
+    num_steps = 10000
+
+    for i in range(num_warmups + num_steps):
+        if i == num_warmups:
+            torch.cuda.synchronize()
+            t = time.perf_counter()
+        out_fused = fused_scatter_reduce(x, index, dim_size=1000,
+                                         reduce_list=['sum', 'mean'])
+    torch.cuda.synchronize()
+    t = time.perf_counter() - t
+    print(f'  Fused implementation: {t:.4f} seconds')
+
+    for i in range(num_warmups + num_steps):
+        if i == num_warmups:
+            torch.cuda.synchronize()
+            t = time.perf_counter()
+        out1 = torch_scatter.scatter_add(x, index, dim_size=1000, dim=0)
+        out2 = torch_scatter.scatter_mean(x, index, dim_size=1000, dim=0)
+        out_vanilla = torch.cat([out1, out2], dim=-1)
+    torch.cuda.synchronize()
+    t = time.perf_counter() - t
+    print(f'Vanilla implementation: {t:.4f} seconds')
+
+    assert torch.allclose(out_fused, out_vanilla, atol=1e-5)
