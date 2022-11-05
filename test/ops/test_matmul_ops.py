901523a963f61407271481fb772ed4c4d819c5da
riship <riship@nvidia.com>
2022-11-05 06:09:12 -0700

    matmul can detect it needs to use bmm under the hood and goes with that

--- test/ops/test_matmul_ops.py
+++ test/ops/test_matmul_ops.py
@@ -11,12 +11,6 @@ test_group_matmul = int(major_vers) >= 2 or int(minor_vers) >= 14
 os.environ['NVIDIA_TF32_OVERRIDE'] = '0'
 
 
-def assert_close_enough(x, y, tol=1e-5):
-    assert ((x - y).abs().max() <=
-            tol), 'Max Abs Err: ' + str(float(
-                (x - y).abs().max())) + ', Tolerance: ' + str(tol)
-
-
 @pytest.mark.parametrize('device_str', DEVICE_STRS)
 def test_segment_matmul_autograd(device_str):
     inputs = torch.randn((8, 16), requires_grad=True, device=device_str)
@@ -24,8 +18,8 @@ def test_segment_matmul_autograd(device_str):
     other = torch.randn((2, 16, 32), requires_grad=True, device=device_str)
     out = pyg_lib.ops.segment_matmul(inputs, ptr, other)
     assert out.shape == (inputs.shape[0], other.shape[-1])
-    assert_close_enough(out[0:ptr[1]], inputs[0:ptr[1]] @ other[0])
-    assert_close_enough(out[ptr[1]:ptr[2]], inputs[ptr[1]:ptr[2]] @ other[1])
+    assert torch.allclose(out[0:ptr[1]], inputs[0:ptr[1]] @ other[0])
+    assert torch.allclose(out[ptr[1]:ptr[2]], inputs[ptr[1]:ptr[2]] @ other[1])
     out.sum().backward()
     assert other.grad.shape == other.shape
     assert inputs.grad.shape == inputs.shape
@@ -50,7 +44,7 @@ def test_grouped_matmul_autograd(device_str):
     assert len(outs) == len(inputs)
     for i in range(len(outs)):
         assert outs[i].size() == (inputs[i].shape[0], others[i].shape[-1])
-        assert_close_enough(outs[i], inputs[i] @ others[i])
+        assert torch.allclose(outs[i], inputs[i] @ others[i])
 
     sum([out.sum() for out in outs]).backward()
     for i in range(len(outs)):
