25f70392b24b3e4d540127eb93ed1a4d5bb185bb
riship <riship@nvidia.com>
2022-11-05 05:31:09 -0700

    matmul can detect it needs to use bmm under the hood and goes with that

--- pyg_lib/csrc/ops/cuda/matmul_kernel.cu
+++ pyg_lib/csrc/ops/cuda/matmul_kernel.cu
@@ -312,10 +312,7 @@ at::Tensor segment_matmul_kernel(const at::Tensor& input,
 #if TORCH_VERSION_MINOR >= 14 or TORCH_VERSION_MAJOR > 1
   auto input_nested = at::_nested_tensor_from_tensor_list(
       input.contiguous().split_with_sizes(/*split_size=*/sizes, /*dim=*/0));
-  auto other_nested = at::_nested_tensor_from_tensor_list(
-                          other.contiguous().split(/*split_size=*/1, /*dim=*/0))
-                          .squeeze(1);
-  auto out_nested = at::matmul(input_nested, other_nested);
+  auto out_nested = at::matmul(input_nested, other.contiguous());
   auto out = at::cat(out_nested.contiguous().unbind());
 #else
   const auto out = input.new_empty({input.size(0), other.size(-1)});
