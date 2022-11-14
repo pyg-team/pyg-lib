7151dcf92f0dc1165a324043b228dfeda33780e2
rusty1s <matthias.fey@tu-dortmund.de>
2022-11-14 13:05:19 +0000

    update

--- .github/workflows/cuda/Linux.sh
+++ .github/workflows/cuda/Linux.sh
@@ -44,7 +44,7 @@ sudo mv cuda-${OS}.pin /etc/apt/preferences.d/cuda-repository-pin-600
 wget -nv ${URL}/${FILENAME}
 sudo dpkg -i ${FILENAME}
 
-if [ "${1}" = "cu177" ]; then
+if [ "${1}" = "cu117" ]; then
   sudo cp /var/cuda-repo-${OS}-${APT_KEY}/cuda-*-keyring.gpg /usr/share/keyrings/
 else
   sudo apt-key add /var/cuda-repo-${APT_KEY}/7fa2af80.pub
