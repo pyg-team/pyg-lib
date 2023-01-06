e88aaa938bdb4f31e60460cfd862e348b2e19943
Rishi Puri <puririshi98@berkeley.edu>
2023-01-06 12:30:43 -0800

    Update test_triton.py

--- test/test_triton.py
+++ test/test_triton.py
@@ -1,16 +1,15 @@
 import torch
 from torch import Tensor
-
 from pyg_lib._triton import tl, triton
 from pyg_lib.testing import onlyCUDA, onlyTriton
 
 
 @triton.jit
-def add_kernel(x_ptr, y_ptr, out_ptr, numel, **meta):
+def add_kernel(x_ptr, y_ptr, out_ptr, numel, BLOCK_SIZE: int):
     pid = tl.program_id(axis=0)
-    block_start = pid * meta['BLOCK_SIZE']
+    block_start = pid * BLOCK_SIZE
 
-    offsets = block_start + tl.arange(0, meta['BLOCK_SIZE'])
+    offsets = block_start + tl.arange(0, BLOCK_SIZE)
     mask = offsets < numel
 
     x = tl.load(x_ptr + offsets, mask=mask)
@@ -24,7 +23,7 @@ def add_kernel(x_ptr, y_ptr, out_ptr, numel, **meta):
 def add(x: Tensor, y: Tensor) -> Tensor:
     out = torch.empty_like(x)
     assert x.is_cuda and y.is_cuda and out.is_cuda
-    grid = lambda meta: (triton.cdiv(x.numel(), meta['BLOCK_SIZE']), )
+    grid = lambda BLOCK_SIZE: (triton.cdiv(x.numel(), BLOCK_SIZE), )
     add_kernel[grid](x, y, out, x.numel(), BLOCK_SIZE=1024)
     return out
 
