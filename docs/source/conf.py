ef340dd21d9f4b22f04355e028efca1e1b3f209c
Matthias Fey <matthias.fey@tu-dortmund.de>
2023-01-10 13:42:13 +0100

    Update Documentation (#173)

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -1,4 +1,8 @@
 import datetime
+import os.path as osp
+import sys
+
+import pyg_sphinx_theme
 
 import pyg_lib
 
@@ -7,12 +11,15 @@ project = 'pyg_lib'
 version = pyg_lib.__version__
 copyright = f'{datetime.datetime.now().year}, {author}'
 
+sys.path.append(osp.join(osp.dirname(pyg_sphinx_theme.__file__), 'extension'))
+
 extensions = [
     'sphinx.ext.autodoc',
     'sphinx.ext.autosummary',
     'sphinx.ext.intersphinx',
     'sphinx.ext.mathjax',
     'sphinx.ext.napoleon',
+    'pyg',
 ]
 
 html_theme = 'pyg_sphinx_theme'
@@ -20,6 +27,8 @@ html_logo = ('https://raw.githubusercontent.com/pyg-team/pyg_sphinx_theme/'
              'master/pyg_sphinx_theme/static/img/pyg_logo.png')
 html_favicon = ('https://raw.githubusercontent.com/pyg-team/pyg_sphinx_theme/'
                 'master/pyg_sphinx_theme/static/img/favicon.png')
+html_static_path = ['_static']
+html_css_files = ['css/custom.css']
 
 add_module_names = False
 autodoc_member_order = 'bysource'
