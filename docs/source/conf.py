a370f733b7d39514177362b7dce8d9cc5d46593f
Matthias Fey <matthias.fey@tu-dortmund.de>
2023-01-15 12:02:27 +0100

    Remove custom CSS (#178)

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -19,6 +19,7 @@ extensions = [
     'sphinx.ext.intersphinx',
     'sphinx.ext.mathjax',
     'sphinx.ext.napoleon',
+    'sphinx.ext.viewcode',
     'pyg',
 ]
 
@@ -27,8 +28,6 @@ html_logo = ('https://raw.githubusercontent.com/pyg-team/pyg_sphinx_theme/'
              'master/pyg_sphinx_theme/static/img/pyg_logo.png')
 html_favicon = ('https://raw.githubusercontent.com/pyg-team/pyg_sphinx_theme/'
                 'master/pyg_sphinx_theme/static/img/favicon.png')
-html_static_path = ['_static']
-html_css_files = ['css/custom.css']
 
 add_module_names = False
 autodoc_member_order = 'bysource'
