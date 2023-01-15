faa7d234329924f5523bfddfa91d168a399be71a
rusty1s <matthias.fey@tu-dortmund.de>
2023-01-15 12:00:30 +0100

    update

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -28,7 +28,6 @@ html_logo = ('https://raw.githubusercontent.com/pyg-team/pyg_sphinx_theme/'
              'master/pyg_sphinx_theme/static/img/pyg_logo.png')
 html_favicon = ('https://raw.githubusercontent.com/pyg-team/pyg_sphinx_theme/'
                 'master/pyg_sphinx_theme/static/img/favicon.png')
-html_static_path = ['_static']
 
 add_module_names = False
 autodoc_member_order = 'bysource'
