c0731e2ca0249e32fbfd15233eb1559e2f8222f9
Rishi Puri <puririshi98@berkeley.edu>
2023-01-18 18:55:14 -0800

    Update matmul_kernel.cu

--- pyg_lib/csrc/ops/cuda/matmul_kernel.cu
+++ pyg_lib/csrc/ops/cuda/matmul_kernel.cu
@@ -103,30 +103,28 @@ int shared_memory_for_kernel() {
 
 // Returns the bytes of shared memory available per SM on the GPU, or -1 on
 // error.
-int shared_memory_per_sm() {
+cudaDeviceProp get_dev_prop() {
   cudaDeviceProp properties;
   int device_idx;
   cudaError_t result = cudaGetDevice(&device_idx);
   if (result != cudaSuccess) {
-    return -1;
+    throw std::runtime_error(cudaGetErrorString(result));
   }
 
   result = cudaGetDeviceProperties(&properties, device_idx);
   if (result != cudaSuccess) {
-    return -1;
+    throw std::runtime_error(cudaGetErrorString(result));
   }
+  return properties;
+}
 
-  return properties.sharedMemPerMultiprocessor;
+if (torch::cuda::is_available()) {
+  cudaDeviceProp props = get_dev_prop();
 }
 
 void grouped_matmul_out_kernel(const at::TensorList input,
                                const at::TensorList other,
                                const at::TensorList out) {
-  cudaDeviceProp props;
-
-  cudaError_t error = cudaGetDeviceProperties(&props, 0);
-  TORCH_CHECK(error == cudaSuccess, cudaGetErrorString(error));
-
   if (props.major < 8) {
     // Compute capability less than that of Ampere. No TF32 available.
     // note: we only support Volta and onwards
@@ -193,8 +191,7 @@ void grouped_matmul_out_kernel(const at::TensorList input,
               >::GemmKernel;
       int grouped_shared_mem =
           shared_memory_for_kernel<DefaultGemmKernel_TF32>();
-      int shared_mem_per_sm = shared_memory_per_sm();
-      if (grouped_shared_mem < shared_mem_per_sm) {
+      if (grouped_shared_mem < props.sharedMemPerMultiprocessor) {
         // full size GPU
         run_grouped_gemm<DefaultGemmKernel_TF32>(input, other, out);
       } else {
@@ -256,8 +253,7 @@ void grouped_matmul_out_kernel(const at::TensorList input,
               >::GemmKernel;
       int grouped_shared_mem =
           shared_memory_for_kernel<DefaultGemmKernel_FP32>();
-      int shared_mem_per_sm = shared_memory_per_sm();
-      if (grouped_shared_mem < shared_mem_per_sm) {
+      if (grouped_shared_mem < props.sharedMemPerMultiprocessor) {
         // full size GPU
         run_grouped_gemm<DefaultGemmKernel_FP32>(input, other, out);
       } else {
