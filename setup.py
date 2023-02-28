00e75095e2653c88c71216a37c3145a94b513fa7
Saurav Maheshkar <sauravvmaheshkar@gmail.com>
2023-02-28 16:09:36 +0530

    fix: add `torch` as a required dependency

--- setup.py
+++ setup.py
@@ -106,7 +106,9 @@ def maybe_append_with_mkl(dependencies):
             dependencies.append(f'mkl-include=={product_version}')
 
 
-install_requires = []
+install_requires = [
+    "torch",
+]
 maybe_append_with_mkl(install_requires)
 
 triton_requires = [
