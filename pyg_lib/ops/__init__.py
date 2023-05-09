e1feb93c7a6f3b6d8c94a9cc54e8e03ed982d9cf
puririshi98 <puririshi98@berkeley.edu>
2023-05-09 14:22:19 -0700

    fixing

--- pyg_lib/ops/__init__.py
+++ pyg_lib/ops/__init__.py
@@ -12,6 +12,9 @@ class GroupedMatmul(Function):
     def forward(ctx, *inputs_and_others):
         ctx.save_for_backward(*(inputs_and_others))
         # autograd complains about list(...) constructor
+        print("len(inputs_and_others)=",len(inputs_and_others))
+        print("inputs_and_others[0]=",inputs_and_others[0])
+        print("len(inputs_and_others[0]))=",len(inputs_and_others[0]))
         inputs: List[Tensor] = [
             i for i in inputs_and_others[:int(len(inputs_and_others) / 2)]
         ]
