83fe04ab53abdbf0b197d768b7da84f38685e75f
rusty1s <matthias.fey@tu-dortmund.de>
2023-05-22 09:07:58 +0000

    changelog

--- test/ops/test_matmul.py
+++ test/ops/test_matmul.py
@@ -1,25 +1,16 @@
 import os
 
-import pytest
 import torch
 
 import pyg_lib
-
-DEVICES = [torch.device('cpu')]
-if torch.cuda.is_available():
-    DEVICES.append(torch.device('cuda'))
+from pyg_lib.testing import withCUDA
 
 os.environ['NVIDIA_TF32_OVERRIDE'] = '0'
 torch.backends.cuda.matmul.allow_tf32 = False
-
-major_vers, minor_vers = str(torch.__version__).split('.')[:2]
-REQ_GRAD = int(major_vers) >= 2 or int(minor_vers) >= 14
-
-if int(minor_vers) >= 12 or int(major_vers) > 1:  # This only exists after 1.12
-    torch.set_float32_matmul_precision('highest')  # Enforce FP32
+torch.set_float32_matmul_precision('highest')  # Enforce FP32
 
 
-@pytest.mark.parametrize('device', DEVICES)
+@withCUDA
 def test_segment_matmul_autograd(device):
     inputs = torch.randn((8, 16), requires_grad=True, device=device)
     ptr = torch.tensor([0, 5, 8]).to(torch.device(device))
@@ -39,7 +30,7 @@ def test_segment_matmul_autograd(device):
     assert inputs.grad.size() == inputs.size()
 
 
-@pytest.mark.parametrize('device', DEVICES)
+@withCUDA
 def test_grouped_matmul_autograd(device):
     inputs = [
         torch.randn(5, 16, device=device),
@@ -47,15 +38,15 @@ def test_grouped_matmul_autograd(device):
         torch.randn(3, 32, device=device),
     ]
     others = [
-        torch.randn(16, 48, device=device, requires_grad=REQ_GRAD),
-        torch.randn(9, 42, device=device, requires_grad=REQ_GRAD),
-        torch.randn(32, 64, device=device, requires_grad=REQ_GRAD),
+        torch.randn(16, 48, device=device, requires_grad=True),
+        torch.randn(9, 42, device=device, requires_grad=True),
+        torch.randn(32, 64, device=device, requires_grad=True),
     ]
 
     biases = [
-        torch.randn(48, device=device, requires_grad=REQ_GRAD),
-        torch.randn(42, device=device, requires_grad=REQ_GRAD),
-        torch.randn(64, device=device, requires_grad=REQ_GRAD),
+        torch.randn(48, device=device, requires_grad=True),
+        torch.randn(42, device=device, requires_grad=True),
+        torch.randn(64, device=device, requires_grad=True),
     ]
 
     outs = pyg_lib.ops.grouped_matmul(inputs, others, biases)
@@ -63,10 +54,9 @@ def test_grouped_matmul_autograd(device):
 
     for i in range(len(outs)):
         assert outs[i].size() == (inputs[i].size(0), others[i].size(-1))
-        assert torch.allclose(outs[i], inputs[i] @ others[i] + biases[i],
-                              atol=1e-6)
+        expected = inputs[i] @ others[i] + biases[i]
+        assert torch.allclose(outs[i], expected, atol=1e-6)
 
-    if REQ_GRAD:
-        sum([out.sum() for out in outs]).backward()
-        for i in range(len(outs)):
-            assert others[i].grad.size() == others[i].size()
+    sum([out.sum() for out in outs]).backward()
+    for i in range(len(outs)):
+        assert others[i].grad.size() == others[i].size()
