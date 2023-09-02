afa89841ee94d6319a5917e65f260b5b42a7854a
kgajdamo <kinga.gajdamowicz@intel.com>
2023-09-02 10:53:39 +0000

    add merge sampler outputs function

---
+++ pyg_lib/csrc/sampler/cpu/dist_merge_outputs_kernel.h
@@ -0,0 +1,25 @@
+#include <ATen/ATen.h>
+#include <torch/library.h>
+#include "pyg_lib/csrc/utils/types.h"
+
+namespace pyg {
+namespace sampler {
+
+std::tuple<at::Tensor,
+           c10::optional<at::Tensor>,
+           c10::optional<at::Tensor>,
+           std::vector<int64_t>>
+merge_sampler_outputs_kernel(
+    const std::vector<at::Tensor>& nodes,
+    const std::vector<std::vector<int64_t>>& cumm_sampled_nbrs_per_node,
+    const std::vector<int64_t>& partition_ids,
+    const std::vector<int64_t>& partition_orders,
+    const int64_t partitions_num,
+    const int64_t one_hop_num,
+    const c10::optional<std::vector<at::Tensor>>& edge_ids,
+    const c10::optional<std::vector<at::Tensor>>& batch,
+    bool disjoint,
+    bool with_edge);
+
+}  // namespace sampler
+}  // namespace pyg
---
+++ pyg_lib/csrc/sampler/dist_merge_outputs.cpp
@@ -0,0 +1,60 @@
+#include "dist_merge_outputs.h"
+
+#include <ATen/core/dispatch/Dispatcher.h>
+#include <torch/library.h>
+
+#include "pyg_lib/csrc/utils/check.h"
+
+namespace pyg {
+namespace sampler {
+
+std::tuple<at::Tensor,
+           c10::optional<at::Tensor>,
+           c10::optional<at::Tensor>,
+           std::vector<int64_t>>
+merge_sampler_outputs(
+    const std::vector<at::Tensor>& nodes,
+    const std::vector<std::vector<int64_t>>& cumm_sampled_nbrs_per_node,
+    const std::vector<int64_t>& partition_ids,
+    const std::vector<int64_t>& partition_orders,
+    const int64_t partitions_num,
+    const int64_t one_hop_num,
+    const c10::optional<std::vector<at::Tensor>>& edge_ids,
+    const c10::optional<std::vector<at::Tensor>>& batch,
+    bool disjoint,
+    bool with_edge) {
+  std::vector<at::TensorArg> nodes_args;
+  pyg::utils::fill_tensor_args(nodes_args, nodes, "nodes", 0);
+
+  at::CheckedFrom c{"merge_sampler_outputs"};
+  at::checkAllDefined(c, {nodes_args});
+
+  TORCH_CHECK(partition_ids.size() == partition_orders.size(),
+              "Each id must be assigned a sampling order'");
+
+  if (disjoint)
+    TORCH_CHECK(batch.has_value(),
+                "I case of disjoint sampling batch needs to be specified");
+
+  if (with_edge)
+    TORCH_CHECK(edge_ids.has_value(), "No edge ids specified");
+
+  static auto op = c10::Dispatcher::singleton()
+                       .findSchemaOrThrow("pyg::merge_sampler_outputs", "")
+                       .typed<decltype(merge_sampler_outputs)>();
+  return op.call(nodes, cumm_sampled_nbrs_per_node, partition_ids,
+                 partition_orders, partitions_num, one_hop_num, edge_ids, batch,
+                 disjoint, with_edge);
+}
+
+TORCH_LIBRARY_FRAGMENT(pyg, m) {
+  m.def(TORCH_SELECTIVE_SCHEMA(
+      "pyg::merge_sampler_outputs(Tensor[] nodes, "
+      "int[][] cumm_sampled_nbrs_per_node, int[] partition_ids, int[] "
+      "partition_orders, int partitions_num, int one_hop_num, Tensor[]? "
+      "edge_ids, Tensor[]? batch, bool disjoint, bool with_edge) -> (Tensor, "
+      "Tensor?, Tensor?, int[])"));
+}
+
+}  // namespace sampler
+}  // namespace pyg
