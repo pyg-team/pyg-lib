9899abe2b3e039ffdbe6b1a8c6bc6bc54b9b76bd
Matthias Fey <matthias.fey@tu-dortmund.de>
2023-10-05 11:41:51 +0200

    Fix CUDA version in Windows build (#259)

--- .github/workflows/cuda/Windows.sh
+++ .github/workflows/cuda/Windows.sh
@@ -8,7 +8,7 @@ curl -k -L "https://drive.google.com/u/0/uc?id=1injUyo3lnarMgWyRcXqKg4UGnN0ysmuq
 case ${1} in
   cu121)
     CUDA_SHORT=12.1
-    CUDA_URL=https://developer.download.nvidia.com/compute/cuda/${CUDA_SHORT}.0/local_installers
+    CUDA_URL=https://developer.download.nvidia.com/compute/cuda/${CUDA_SHORT}.1/local_installers
     CUDA_FILE=cuda_${CUDA_SHORT}.1_531.14_windows.exe
     ;;
   cu118)
