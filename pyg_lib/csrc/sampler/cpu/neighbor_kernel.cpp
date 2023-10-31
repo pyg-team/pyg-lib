cf440e89ce0c6f19b1043a9e83f91d2ea3795b7d
OlhaBabicheva <olhababicheva2002@gmail.com>
2023-10-31 08:44:47 +0000

    Added comments

--- pyg_lib/csrc/sampler/cpu/neighbor_kernel.cpp
+++ pyg_lib/csrc/sampler/cpu/neighbor_kernel.cpp
@@ -224,7 +224,7 @@ class NeighborSampler {
     // Case 2: Multinomial sampling:
     else {
       at::Tensor index;
-      if (replace) {
+      if (replace) { // at::multinomial has good perfomance only when replace=true, e.g. https://github.com/pytorch/pytorch/issues/11931#top
         index = at::multinomial(weight, count, replace);
       }
       else { // An Efficient Algorithm for Biased Sampling: https://utopia.duth.gr/~pefraimi/research/data/2007EncOfAlg.pdf
