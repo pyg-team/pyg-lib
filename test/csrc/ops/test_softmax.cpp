651045ef96a9f6e78edf44400cb125421ce64d8f
Damian Szwichtenberg <damian.szwichtenberg@intel.com>
2023-11-20 11:37:47 +0100

    Move `softmax_csr` autograd module to C++

--- test/csrc/ops/test_softmax.cpp
+++ test/csrc/ops/test_softmax.cpp
@@ -40,7 +40,7 @@ TEST_P(CPUTest, SoftmaxCSRForward) {
   EXPECT_TRUE(at::allclose(expected_out, out, 1e-04, 1e-04));
 }
 
-TEST_P(CPUTest, SoftmaxCSRBackward) {
+TEST_P(CPUTest, SoftmaxCSRAutogradBackward) {
   const auto dim = ::testing::TestWithParam<int64_t>::GetParam();
   const auto src = at::rand({8, 8});
   src.set_requires_grad(true);
@@ -48,11 +48,21 @@ TEST_P(CPUTest, SoftmaxCSRBackward) {
   const auto out = softmax2D_ref_impl(src, ptr, dim);
   const auto out_grad = at::rand({8, 8});
 
+  // use softmax_csr_backward directly
   const auto in_grad = pyg::ops::softmax_csr_backward(out, out_grad, ptr, dim);
   out.backward(out_grad);
   EXPECT_EQ(src.grad().size(0), in_grad.size(0));
   EXPECT_EQ(src.grad().size(1), in_grad.size(1));
   EXPECT_TRUE(at::allclose(src.grad(), in_grad, 1e-04, 1e-04));
+
+  // use softmax backward via autograd module
+  const auto src2 = src.detach().clone();
+  src2.set_requires_grad(true);
+  const auto out2 = pyg::ops::softmax_csr_forward(src2, ptr, dim);
+  out2.backward(out_grad);
+  EXPECT_EQ(src.grad().size(0), src2.grad().size(0));
+  EXPECT_EQ(src.grad().size(1), src2.grad().size(1));
+  EXPECT_TRUE(at::allclose(src.grad(), src2.grad(), 1e-04, 1e-04));
 }
 
 INSTANTIATE_TEST_SUITE_P(OpsTest,
