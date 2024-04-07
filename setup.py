332c064356ba3e9c75ce16d0248e7289860ab57f
rusty1s <matthias.fey@tu-dortmund.de>
2024-04-07 07:07:31 +0000

    update

--- setup.py
+++ setup.py
@@ -66,6 +66,10 @@ class CMakeBuild(build_ext):
             f'-DCMAKE_PREFIX_PATH={torch.utils.cmake_prefix_path}',
         ]
 
+        cuda_arch_list = os.getenv('TORCH_CUDA_ARCH_LIST')
+        if WITH_CUDA and cuda_arch_list is not None:
+            cmake_args.append(f'-DCMAKE_CUDA_ARCHITECTURES={cuda_arch_list}')
+
         if CMakeBuild.check_env_flag('USE_MKL_BLAS'):
             include_dir = f"{sysconfig.get_path('data')}{os.sep}include"
             cmake_args.append(f'-DBLAS_INCLUDE_DIR={include_dir}')
