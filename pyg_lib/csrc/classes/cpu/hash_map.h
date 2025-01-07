78f07dc496a57bd134bf966718349c143d79a694
Matthias Fey <matthias.fey@tu-dortmund.de>
2025-01-07 09:56:49 +0100

    Basic implementation of `CPUHashMap` (#375)
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>

--- pyg_lib/csrc/classes/cpu/hash_map.h
+++ pyg_lib/csrc/classes/cpu/hash_map.h
@@ -5,12 +5,39 @@
 namespace pyg {
 namespace classes {
 
+template <typename T>
 struct CPUHashMap : torch::CustomClassHolder {
-  CPUHashMap() {};
+  std::unordered_map<T, int64_t> map;
+
+  CPUHashMap(const at::Tensor& key) {
+    // TODO Assert 1-dim
+    const auto key_data = key.data_ptr<T>();
+    for (int64_t i = 0; i < key.numel(); ++i) {
+      // TODO Check that key does not yet exist.
+      map[key_data[i]] = i;
+    }
+  };
+
+  at::Tensor get(const at::Tensor& query) {
+    // TODO Assert 1-dim
+    const auto options = at::TensorOptions().dtype(at::kLong);
+    auto out = at::empty({query.numel()}, options);
+
+    const auto query_data = query.data_ptr<T>();
+    auto out_data = out.data_ptr<int64_t>();
+
+    for (size_t i = 0; i < query.numel(); ++i) {
+      // TODO Insert -1 if key does not exist.
+      out_data[i] = map[query_data[i]];
+    }
+    return out;
+  }
 };
 
 TORCH_LIBRARY(pyg, m) {
-  m.class_<CPUHashMap>("CPUHashMap").def(torch::init());
+  m.class_<CPUHashMap<int64_t>>("CPULongHashMap")
+      .def(torch::init<at::Tensor&>())
+      .def("get", &CPUHashMap<int64_t>::get);
 }
 
 }  // namespace classes
--- test/csrc/classes/test_hash_map.cpp
+++ test/csrc/classes/test_hash_map.cpp
@@ -1,7 +1,15 @@
+#include <ATen/ATen.h>
 #include <gtest/gtest.h>
 
 #include "pyg_lib/csrc/classes/cpu/hash_map.h"
 
 TEST(CPUHashMapTest, BasicAssertions) {
-  auto map = pyg::classes::CPUHashMap();
+  auto options = at::TensorOptions().dtype(at::kLong);
+  auto key = at::tensor({0, 10, 30, 20}, options);
+
+  auto map = pyg::classes::CPUHashMap<int64_t>(key);
+
+  auto query = at::tensor({30, 10, 20}, options);
+  auto expected = at::tensor({2, 1, 3}, options);
+  EXPECT_TRUE(at::equal(map.get(query), expected));
 }
