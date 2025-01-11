9bd43a0bfb7d13fcdd25b13caae77baaa0f828ad
rusty1s <matthias.fey@tu-dortmund.de>
2025-01-11 12:17:51 +0000

    update

--- pyg_lib/classes/__init__.py
+++ pyg_lib/classes/__init__.py
@@ -1,6 +1,17 @@
 import torch
+from torch import Tensor
 
-HashMap = torch.classes.pyg.CPUHashMap
+
+class HashMap:
+    def __init__(self, key: Tensor) -> Tensor:
+        self._map = torch.classes.pyg.CPUHashMap(key)
+
+    def get(self, query: Tensor) -> Tensor:
+        return self._map.get(query)
+
+    def __repr__(self) -> str:
+        return f'{self.__class__.__name__}()'
+
 
 __all__ = [
     'HashMap',
