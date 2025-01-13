1c9d3e14fa7c2a81abca18360399c2d7a591ffea
rusty1s <matthias.fey@tu-dortmund.de>
2025-01-13 18:54:41 +0000

    update

--- test/csrc/classes/test_hash_map.cpp
+++ test/csrc/classes/test_hash_map.cpp
@@ -1,15 +1,15 @@
 #include <ATen/ATen.h>
 #include <gtest/gtest.h>
 
-/* #include "pyg_lib/csrc/classes/hash_map.h" */
+#include "pyg_lib/csrc/classes/hash_map.h"
 
 TEST(HashMapTest, BasicAssertions) {
-  /* auto options = at::TensorOptions().dtype(at::kLong); */
-  /* auto key = at::tensor({0, 10, 30, 20}, options); */
+  auto options = at::TensorOptions().dtype(at::kLong);
+  auto key = at::tensor({0, 10, 30, 20}, options);
 
-  /* auto map = pyg::classes::HashMap(key); */
+  auto map = pyg::classes::HashMap(key);
 
-  /* auto query = at::tensor({30, 10, 20, 40}, options); */
-  /* auto expected = at::tensor({2, 1, 3, -1}, options); */
-  /* EXPECT_TRUE(at::equal(map.get(query), expected)); */
+  auto query = at::tensor({30, 10, 20, 40}, options);
+  auto expected = at::tensor({2, 1, 3, -1}, options);
+  EXPECT_TRUE(at::equal(map.get(query), expected));
 }
