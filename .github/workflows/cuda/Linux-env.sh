a739b95260bfcc921028c4fd02d7b17ee290fc6d
rusty1s <matthias.fey@tu-dortmund.de>
2025-04-26 22:03:41 +0200

    update

--- .github/workflows/cuda/Linux-env.sh
+++ .github/workflows/cuda/Linux-env.sh
@@ -2,50 +2,46 @@
 
 case ${1} in
   cu126)
-    export FORCE_CUDA=1
-    export PATH=/usr/local/cuda-12.6/bin:${PATH}
+    export CUDA_HOME=/usr/local/cuda-12.6
     export TORCH_CUDA_ARCH_LIST="6.0+PTX;7.0;7.5;8.0;8.6;9.0"
     ;;
   cu124)
-    export FORCE_CUDA=1
-    export PATH=/usr/local/cuda-12.4/bin:${PATH}
+    export CUDA_HOME=/usr/local/cuda-12.4
     export TORCH_CUDA_ARCH_LIST="6.0+PTX;7.0;7.5;8.0;8.6;9.0"
     ;;
   cu121)
-    export FORCE_CUDA=1
-    export PATH=/usr/local/cuda-12.1/bin:${PATH}
+    export CUDA_HOME=/usr/local/cuda-12.1
     export TORCH_CUDA_ARCH_LIST="6.0+PTX;7.0;7.5;8.0;8.6;9.0"
     ;;
   cu118)
-    export FORCE_CUDA=1
-    export PATH=/usr/local/cuda-11.8/bin:${PATH}
+    export CUDA_HOME=/usr/local/cuda-11.8
     export TORCH_CUDA_ARCH_LIST="6.0+PTX;7.0;7.5;8.0;8.6;9.0"
     ;;
   cu117)
-    export FORCE_CUDA=1
-    export PATH=/usr/local/cuda-11.7/bin:${PATH}
+    export CUDA_HOME=/usr/local/cuda-11.7
     export TORCH_CUDA_ARCH_LIST="6.0+PTX;7.0;7.5;8.0;8.6"
     ;;
   cu116)
-    export FORCE_CUDA=1
-    export PATH=/usr/local/cuda-11.6/bin:${PATH}
+    export CUDA_HOME=/usr/local/cuda-11.6
     export TORCH_CUDA_ARCH_LIST="6.0+PTX;7.0;7.5;8.0;8.6"
     ;;
   cu115)
-    export FORCE_CUDA=1
-    export PATH=/usr/local/cuda-11.5/bin:${PATH}
+    export CUDA_HOME=/usr/local/cuda-11.5
     export TORCH_CUDA_ARCH_LIST="6.0+PTX;7.0;7.5;8.0;8.6"
     ;;
   cu113)
-    export FORCE_CUDA=1
-    export PATH=/usr/local/cuda-11.3/bin:${PATH}
+    export CUDA_HOME=/usr/local/cuda-11.3
     export TORCH_CUDA_ARCH_LIST="6.0+PTX;7.0;7.5;8.0;8.6"
     ;;
   cu102)
-    export FORCE_CUDA=1
-    export PATH=/usr/local/cuda-10.2/bin:${PATH}
+    export CUDA_HOME=/usr/local/cuda-10.2
     export TORCH_CUDA_ARCH_LIST="6.0+PTX;7.0;7.5"
     ;;
   *)
+    export FORCE_CUDA=0
+    exit 0
     ;;
 esac
+
+export FORCE_CUDA=1
+export PATH=${CUDA_HOME}/bin:${PATH}
