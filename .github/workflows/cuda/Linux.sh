3d1320a025a4cab0cfd55a074c1c149674128adf
rusty1s <matthias.fey@tu-dortmund.de>
2025-04-27 09:47:12 +0200

    update

--- .github/workflows/cuda/Linux.sh
+++ .github/workflows/cuda/Linux.sh
@@ -66,7 +66,7 @@ case ${1} in
     ;;
 esac
 
-apt-get install -y wget
+sudo apt-get install -y wget
 wget -nv https://developer.download.nvidia.com/compute/cuda/repos/${OS}/x86_64/cuda-${OS}.pin
 sudo mv cuda-${OS}.pin /etc/apt/preferences.d/cuda-repository-pin-600
 wget -nv ${URL}/${FILENAME}
@@ -81,5 +81,6 @@ fi
 sudo apt-get -qq update
 sudo apt install cuda-nvcc-${CUDA/./-} cuda-libraries-dev-${CUDA/./-} cuda-command-line-tools-${CUDA/./-}
 sudo apt clean
+sudo ln -s /usr/local/cuda-${CUDA} /usr/local/cuda
 
 rm -f ${FILENAME}
