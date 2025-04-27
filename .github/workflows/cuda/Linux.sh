c5a6b43c5f2bd0ddcf0b00f999d3d5031118e4ac
rusty1s <matthias.fey@tu-dortmund.de>
2025-04-27 18:29:36 +0200

    update

--- .github/workflows/cuda/Linux.sh
+++ .github/workflows/cuda/Linux.sh
@@ -67,7 +67,7 @@ case ${1} in
 esac
 
 if [ "$CIBUILDWHEEL" = "1" ]; then
-  apt-get install -y wget
+  yum install -y wget
   wget https://developer.download.nvidia.com/compute/cuda/12.6.3/local_installers/cuda_12.6.3_560.35.05_linux.run
   sh cuda_12.6.3_560.35.05_linux.run
 else
