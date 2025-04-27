9b181d539a381588406d65d799e414d010432d0a
rusty1s <matthias.fey@tu-dortmund.de>
2025-04-27 18:45:24 +0200

    update

--- .github/workflows/cuda/Linux.sh
+++ .github/workflows/cuda/Linux.sh
@@ -69,7 +69,7 @@ esac
 if [ "$CIBUILDWHEEL" = "1" ]; then
   yum install -y wget
   wget https://developer.download.nvidia.com/compute/cuda/12.6.3/local_installers/cuda_12.6.3_560.35.05_linux.run
-  sh cuda_12.6.3_560.35.05_linux.run
+  sh cuda_12.6.3_560.35.05_linux.run --silent --toolkit
 else
   wget -nv https://developer.download.nvidia.com/compute/cuda/repos/${OS}/x86_64/cuda-${OS}.pin
   sudo mv cuda-${OS}.pin /etc/apt/preferences.d/cuda-repository-pin-600
