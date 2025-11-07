ee2dd39f1b6394a23bd18d23fe545a1b0b9926c6
jiinyih <tanjiinyih4@gmail.com>
2025-11-07 15:45:25 +0800

    Added index_sort.h

--- pyg_lib/csrc/ops/index_sort.cpp
+++ pyg_lib/csrc/ops/index_sort.cpp
@@ -1,8 +1,24 @@
+#include "index_sort.h"
+
 #include <ATen/core/dispatch/Dispatcher.h>
 #include <torch/library.h>
 
 namespace pyg {
 namespace ops {
+
+// I think this is needed as well?
+PYG_API std::tuple<at::Tensor, at::Tensor> index_sort(
+    const at::Tensor& input,
+    const at::optional<int64_t> max) {
+
+
+  static auto op = c10::Dispatcher::singleton()
+                       .findSchemaOrThrow("pyg::index_sort", "")
+                       .typed<decltype(index_sort)>();
+  
+  return op.call(input, max);
+}
+  
 
 TORCH_LIBRARY_FRAGMENT(pyg, m) {
   m.def(TORCH_SELECTIVE_SCHEMA(
---
+++ pyg_lib/csrc/ops/index_sort.h
@@ -0,0 +1,19 @@
+#pragma once
+
+#include <ATen/ATen.h>
+#include <torch/script.h>
+#include "pyg_lib/csrc/macros.h"
+
+namespace pyg {
+namespace ops {
+
+// Im just following the format given in cpu/index_sort_kernal.cpp
+// and the format of the other headers
+PYG_API std::tuple<at::Tensor, at::Tensor> index_sort(
+    const at::Tensor& input,
+    const at::optional<int64_t> max);
+
+
+
+} // namespace ops 
+} // namespace pyg
