e8090f75961a0ba78c9c51cfa2bb798b7cc367dd
rusty1s <matthias.fey@tu-dortmund.de>
2026-03-05 13:30:47 +0000

    update

--- .github/workflows/aws/upload_nightly_index.py
+++ .github/workflows/aws/upload_nightly_index.py
@@ -41,6 +41,8 @@ for obj in bucket.objects.filter(Prefix='whl/nightly'):
         wheels_dict[torch_version.replace('2.5.0', '2.5.1')].append(wheel)
     if '2.7.0' in torch_version:
         wheels_dict[torch_version.replace('2.7.0', '2.7.1')].append(wheel)
+    if '2.9.0' in torch_version:
+        wheels_dict[torch_version.replace('2.9.0', '2.9.1')].append(wheel)
 
 index_html = html.format('\n'.join([
     href.format(f'{version}.html'.replace('+', '%2B'), version)
