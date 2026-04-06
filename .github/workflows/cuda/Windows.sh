daa7fb42ceae3f612218ec9e60cfd572e57508f0
Akihiro Nitta <nitta@akihironitta.com>
2026-04-06 08:59:08 -0700

    feat: Support PyTorch 2.12 nightly on Windows (#630)

--- .github/workflows/cuda/Windows.sh
+++ .github/workflows/cuda/Windows.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 
 case ${1} in
+  cu132)
+    CUDA_SHORT=13.2
+    CUDA_URL=https://developer.download.nvidia.com/compute/cuda/${CUDA_SHORT}.0/local_installers
+    CUDA_FILE=cuda_${CUDA_SHORT}.0_windows.exe
+    ;;
   cu130)
     CUDA_SHORT=13.0
     CUDA_URL=https://developer.download.nvidia.com/compute/cuda/${CUDA_SHORT}.0/local_installers
@@ -65,8 +70,8 @@ esac
 curl -k -L "${CUDA_URL}/${CUDA_FILE}" --output "${CUDA_FILE}"
 echo ""
 echo "Installing from ${CUDA_FILE}..."
-if [ "${1}" = "cu130" ]; then
-  # CUDA 13.0 uses different component naming; install all toolkit components
+if [ "${1}" = "cu132" ] || [ "${1}" = "cu130" ]; then
+  # CUDA 13.* uses different component naming; install all toolkit components
   PowerShell -Command "Start-Process -FilePath \"${CUDA_FILE}\" -ArgumentList \"-s\" -Wait -NoNewWindow"
 else
   COMPONENTS="nvcc_${CUDA_SHORT} cuobjdump_${CUDA_SHORT} nvprune_${CUDA_SHORT} cupti_${CUDA_SHORT} cublas_dev_${CUDA_SHORT} cudart_${CUDA_SHORT} cufft_dev_${CUDA_SHORT} curand_dev_${CUDA_SHORT} cusolver_dev_${CUDA_SHORT} cusparse_dev_${CUDA_SHORT} thrust_${CUDA_SHORT} npp_dev_${CUDA_SHORT} nvrtc_dev_${CUDA_SHORT} nvml_dev_${CUDA_SHORT}"
