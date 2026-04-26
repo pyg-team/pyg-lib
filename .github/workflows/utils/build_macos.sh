dde4f2e52afe8181f590b478ad37f4b8a1a3bbdd
Akihiro Nitta <nitta@akihironitta.com>
2026-04-26 15:38:42 -0700

    CI: Build with PyTorch RC (#641)

--- .github/workflows/utils/build_macos.sh
+++ .github/workflows/utils/build_macos.sh
@@ -8,9 +8,12 @@ echo "TORCH_VERSION: ${TORCH_VERSION}"
 
 export CIBW_BUILD="cp${PYTHON_VERSION//./}-macosx_arm64"
 # pyg-lib doesn't have torch as a dependency, so we need to explicitly install it when running tests.
-if [[ "${TORCH_VERSION}" == "2.12.0" ]]; then
+if [[ "${TORCH_VERSION}" == "2.13.0" ]]; then
   export CIBW_BEFORE_BUILD="pip install ninja wheel setuptools && pip install --pre torch --index-url https://download.pytorch.org/whl/nightly/cpu"
   export CIBW_BEFORE_TEST="pip install pytest && pip install --pre torch --index-url https://download.pytorch.org/whl/nightly/cpu"
+elif [[ "${TORCH_VERSION}" == "2.12.0" ]]; then
+  export CIBW_BEFORE_BUILD="pip install ninja wheel setuptools && pip install torch==${TORCH_VERSION} --index-url https://download.pytorch.org/whl/test/cpu"
+  export CIBW_BEFORE_TEST="pip install pytest && pip install torch==${TORCH_VERSION} --index-url https://download.pytorch.org/whl/test/cpu"
 else
   export CIBW_BEFORE_BUILD="pip install ninja wheel setuptools && pip install torch==${TORCH_VERSION} --index-url https://download.pytorch.org/whl/cpu"
   export CIBW_BEFORE_TEST="pip install pytest && pip install torch==${TORCH_VERSION} --index-url https://download.pytorch.org/whl/cpu"
