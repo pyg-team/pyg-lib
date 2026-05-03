f8e1c73b167d90d4e216f3037cee342dc4ee7264
Akihiro Nitta <nitta@akihironitta.com>
2026-05-03 22:32:55 +0000

    update

--- .github/workflows/utils/build_linux.sh
+++ .github/workflows/utils/build_linux.sh
@@ -12,7 +12,8 @@ source ./.github/workflows/cuda/Linux-env.sh ${CUDA_VERSION}
 echo "FORCE_CUDA: ${FORCE_CUDA}"
 echo "TORCH_CUDA_ARCH_LIST: ${TORCH_CUDA_ARCH_LIST}"
 
-export CIBW_BUILD="cp${PYTHON_VERSION//./}-manylinux_x86_64"
+ARCH=$(uname -m)
+export CIBW_BUILD="cp${PYTHON_VERSION//./}-manylinux_${ARCH}"
 # pyg-lib doesn't have torch as a dependency, so we need to explicitly install it when running tests.
 if [[ "${TORCH_VERSION}" == "2.13.0" ]]; then
   export CIBW_BEFORE_BUILD="pip install ninja wheel setuptools && pip install --pre torch --index-url https://download.pytorch.org/whl/nightly/${CUDA_VERSION}"
@@ -27,10 +28,12 @@ fi
 
 if [[ "${CUDA_VERSION}" == "cu"* ]]; then
   # Use CUDA-pre-installed image
-  export CIBW_MANYLINUX_X86_64_IMAGE=ghcr.io/pyg-team/pyg-lib/manylinux_2_28_x86_64:${CUDA_VERSION}
+  IMAGE=ghcr.io/pyg-team/pyg-lib/manylinux_2_28_${ARCH}:${CUDA_VERSION}
 else
-  export CIBW_MANYLINUX_X86_64_IMAGE=quay.io/pypa/manylinux_2_28_x86_64
+  IMAGE=quay.io/pypa/manylinux_2_28_${ARCH}
 fi
+export CIBW_MANYLINUX_X86_64_IMAGE=$IMAGE
+export CIBW_MANYLINUX_AARCH64_IMAGE=$IMAGE
 
 rm -rf Testing pyg_lib/libpyg.so build dist outputs  # for local testing
 python -m cibuildwheel --output-dir dist
