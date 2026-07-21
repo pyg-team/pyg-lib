dd3f379e1e9552a63063a0475075603777671bad
Akihiro Nitta <nitta@akihironitta.com>
2026-07-21 06:15:35 +0000

    update

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ import warnings
 from setuptools import Extension, find_packages, setup
 from setuptools.command.build_ext import build_ext
 
-__version__ = '0.8.0'
+__version__ = '0.9.0'
 URL = 'https://github.com/pyg-team/pyg-lib'
 
 
